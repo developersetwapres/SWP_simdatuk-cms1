2d53ad8825b7b0783844f3bd4fc4c5d20887b1da
Ari Suharsa <ari@ekuator.id>
2024-03-07 20:42:52 +0700

    feat: add login component

--- components/auth/LoginComponent.jsx
+++ components/auth/LoginComponent.jsx
@@ -1,71 +1,49 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@mui/styles'
-import { Alert, Box, Container, Grid } from '@mui/material'
+/* eslint-disable no-unused-vars */
+import React, { useState, useSelector } from 'react'
+import {
+  Grid, Box, Container
+} from '@mui/material'
+import PropTypes from 'prop-types'
+import LoginContentText from '../simdatuk/auth/login/LoginContentText'
+import LoginModal from './LoginModal'
+import Image from 'next/image'
+import authHero from '/public/simdatuk/authHero.png'
 import LoginFormComponent from './LoginFormComponent'
 import { useForm } from '@/hooks/index'
-import Footer from '../core/Footer'
-import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
-import LoginModal from './LoginModal'
-import { notAllowedCharAndWord } from '@/utils/regex'
-import { setStorages, getStorage } from '@/utils/storage'
-import Login2FAModal from './Login2FAModal'
+// import { setStorages, getStorage } from '@/utils/storage'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    overflow: 'auto hidden',
-    backgroundColor: '#2F2F2F',
-    color: '#fff',
-    height: '100vh'
-  },
-  child: {
-    marginTop: '100px',
-    // marginBottom: '10px',
-    [theme.breakpoints.down('sm')]: {
-      marginTop: '50px'
-      // marginBottom: '35px'
-    }
-  }
-}))
-
-function LoginComponent({
+const LoginComponent = ({
   authentication = () => { },
-  forgetPassword = () => { },
-  qrCode = () => { }
-}) {
-  const errorState = useSelector(state => state.authentication)
-  const rememberMeState = getStorage('remember_setneg') ? JSON.parse(getStorage('remember_setneg')) : null
-  // eslint-disable-next-line no-unused-vars
+  forgetPassword = () => { }
+}) => {
+
+  const errorState = ''
+  // const rememberMeState = getStorage('remember_setneg') ? JSON.parse(getStorage('remember_setneg')) : null
+
+
   const [initialValues, setInitialValues] = useState({
-    nip: rememberMeState?.nip === '' ? '' : rememberMeState?.nip,
-    password: rememberMeState?.password === '' ? '' : rememberMeState?.password
+    // username: rememberMeState?.username === '' ? '' : rememberMeState?.username,
+    // password: rememberMeState?.password === '' ? '' : rememberMeState?.password
+    username: '',
+    password: ''
   })
 
-  const [rememberMe, setRememberMe] = useState(rememberMeState?.isRemember || false)
+  // const [rememberMe, setRememberMe] = useState(rememberMeState?.isRemember || false)
 
   const [modalResetEmail, setModalResetEmail] = useState(false)
   const [modalResetEmailFinish, setResetEmailFinish] = useState(false)
   const [modal2FA, setModal2FA] = useState(false)
 
 
-  const classes = useStyles()
+  // const classes = useStyles()
 
   const validate = (fieldOfValues = values) => {
     const temp = { ...errors }
 
-    if ('nip' in fieldOfValues) {
-      temp.nip = fieldOfValues.nip
-        ? (
-          notAllowedCharAndWord(fieldOfValues.nip)
-            ? (
-              fieldOfValues.nip.length <= 18
-                ? ''
-                : 'SSO tidak boleh lebih dari 18'
-            )
-            : 'SSO tidak boleh mengandung simbol atau huruf'
-        )
-        : 'SSO tidak boleh kosong'
+    if ('username' in fieldOfValues) {
+      temp.username = fieldOfValues.username
+        ? ''
+        : 'username tidak boleh kosong'
     }
 
 
@@ -89,29 +67,32 @@ function LoginComponent({
     handleInputChange
   } = useForm(initialValues, true, validate)
 
+
+  console.log('values', values)
   const handleLogin = (e) => {
     e.preventDefault()
     if (validate()) {
       const payload = {
-        nip: values.nip,
+        username: values.username,
         password: values.password
       }
-      if (rememberMe === true) {
-        setStorages([
-          {
-            name: 'remember_setneg',
-            value: JSON.stringify({
-              nip: values.nip,
-              password: values.password,
-              isRemember: true
-            })
-          }
-        ])
-      } else {
-        localStorage.removeItem('remember_setneg')
-      }
-      // authentication(payload)
-      qrCode(payload)
+      // if (rememberMe === true) {
+      //   setStorages([
+      //     {
+      //       name: 'remember_setneg',
+      //       value: JSON.stringify({
+      //         username: values.username,
+      //         password: values.password,
+      //         isRemember: true
+      //       })
+      //     }
+      //   ])
+      // }
+      //    else {
+      //   localStorage.removeItem('remember_setneg')
+      // }
+      authentication(payload)
+      // qrCode(payload)
 
       if (errorState?.statusCode === 200) {
         setModal2FA(true)
@@ -121,6 +102,9 @@ function LoginComponent({
 
     }
   }
+
+
+
 
   const handleResetEmail = () => {
     setModalResetEmail(true)
@@ -132,126 +116,102 @@ function LoginComponent({
   }
 
 
-  useEffect(() => {
-    if (errorState?.forgetStatus === 'SUCCESS') {
-      setResetEmailFinish(true)
-      setModalResetEmail(false)
-    } else if (errorState?.forgetStatus === 'FAILED') {
-      setResetEmailFinish(false)
-      setModalResetEmail(true)
-    }
-  }, [errorState])
+
 
   return (
-    <Box
-      component='div'
-      className={classes.root}
-    >
+    <Box sx={{
+      display: 'flex',
+      height: '100vh',
+      width: '100vw',
+      justifyContent: 'center',
+      alignItems: {
+        xs: 'start',
+        sm: 'center',
+        md: 'center',
+        lg: 'center',
+        xl: 'center'
+      }
+    }}>
+      <Box>
+        <Image
+          src={authHero}
+          layout='fill'
+          alt='background'
+        />
+      </Box>
       <Container
-        maxWidth='sm'
-        className={classes.child}
-      >
+        sx={{
+          height: '60%',
+          position: 'relative',
+          zIndex: 100
+        }}>
         <Grid
           container
-          direction='column'
-        >
+          columnSpacing={5}
+          sx={{
+            height: '100%',
+            alignItems: 'center'
+
+          }}>
           <Grid
             item
-            lg={12}
-            xl={12}
-            md={12}
-            sm={12}
             xs={12}
+            sm={6}
+            md={6}
             sx={{
-              marginTop: '-70px'
-            }}
-          >
-            <img
-              src='/images/Logo_Setneg_White.svg'
-              alt='logo'
-              style={{
-                width: '100%',
-                maxWidth: '460px',
-                height: '98px',
-                display: 'block',
-                margin: '40px auto'
-              }}
-            />
-          </Grid>
-          <Grid
-            item
-          >
-            {
-              errorState?.error?.code > 400 && (
-                <Alert
-                  severity='error'
-                  sx={{
-                    marginBottom: '20px'
-                  }}
-                >
-                  {errorState?.error?.message}
-                </Alert>
-              )
-            }
-          </Grid>
-          <Grid
-            item
-            sx={{
-              marginTop: {
-                xs: '10px',
-                sm: '10px'
+              paddingBottom: {
+                xs: '20px',
+                md: '40px'
+              },
+              paddingTop: {
+                xs: '30px',
+                md: '40px'
               }
             }}
           >
-            <LoginFormComponent
-              values={values}
-              errors={errors}
-              handleInputChange={handleInputChange}
-              handleLogin={handleLogin}
-              handleResetEmail={handleResetEmail}
-              stateLoading={errorState}
-              rememberMe={rememberMe}
-              setRememberMe={setRememberMe}
-            />
+            {/* Text  Banner*/}
+            <LoginContentText />
+            {/* end Text Banner */}
           </Grid>
           <Grid
             item
-            sx={{
-              marginTop: {
-                xl: errorState?.error ? '130px' : '140px',
-                lg: errorState?.error ? '130px' : '140px',
-                md: errorState?.error ? '100px' : '140px',
-                sm: errorState?.erorr ? '100px' : '200px',
-                xs: errorState?.error ? '60px' : '140px'
-              }
-            }}
+            xs={12}
+            sm={6}
+            md={6}
           >
-            <Footer
+            <Box
               sx={{
-                color: '#fff'
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'end'
               }}
-            />
+            >
+              {/* Form Component */}
+              <LoginFormComponent
+                values={values}
+                errors={errors}
+                handleInputChange={handleInputChange}
+                handleLogin={handleLogin}
+                handleResetEmail={handleResetEmail}
+                stateLoading={errorState}
+              // rememberMe={rememberMe}
+              // setRememberMe={setRememberMe}
+              />
+              {/* End Form Component */}
+
+              {/* Modal Reset email */}
+              <LoginModal
+                openModal={modalResetEmail}
+                finishModal={modalResetEmailFinish}
+                setResetEmail={setModalResetEmail}
+                forgetPassword={forgetPassword}
+                handleCloseModal={handleCloseModal}
+              />
+              {/* End Modal Reset Email */}
+            </Box>
           </Grid>
         </Grid>
-        {/* Modal Reset email */}
-        <LoginModal
-          openModal={modalResetEmail}
-          finishModal={modalResetEmailFinish}
-          setResetEmail={setModalResetEmail}
-          forgetPassword={forgetPassword}
-          handleCloseModal={handleCloseModal}
-        />
-        {/* End Modal Reset Email */}
-        {/* Start Modal OTP */}
-        <Login2FAModal
-          qrCode={errorState?.qrCode}
-          authentication={authentication}
-          value={values}
-          modal={modal2FA}
-          setModal={setModal2FA}
-          errorState={errorState}
-        />
-        {/* End Modal OTP */}
       </Container>
     </Box >
   )
@@ -259,8 +219,9 @@ function LoginComponent({
 
 LoginComponent.propTypes = {
   authentication: PropTypes.func,
-  forgetPassword: PropTypes.func,
-  qrCode: PropTypes.func
+  forgetPassword: PropTypes.func
 }
 
 export default LoginComponent
+
+
