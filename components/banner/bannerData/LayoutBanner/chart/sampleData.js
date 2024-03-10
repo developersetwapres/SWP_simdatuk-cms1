22a1514ec7e3e7784aade8b60fc178081adb9005
Ari Suharsa <ari@ekuator.id>
2024-03-10 19:40:08 +0700

    chore(dashboard): employee dummy data

---
+++ components/banner/bannerData/LayoutBanner/chart/sampleData.js
@@ -0,0 +1,38 @@
+export const dataPegawai = [
+  {
+    id: 1,
+    name: 'Kepala Sekretariat Wakil Presiden',
+    employee: 1,
+    color:'#E84336'
+  },
+  {
+    id: 2,
+    name: 'Deputi Bidang Dukungan Kebijakan Pembangunan Ekonomi dan Peningkatan Daya Saing',
+    employee: 24,
+    color:'#F8A232'
+  },
+  {
+    id: 3,
+    name: 'Deputi Bidang Dukungan Kebijakan Pembangunan Manusia dan Pemerataan Pembangunan',
+    employee: 26,
+    color:'#f5ee4b'
+  },
+  {
+    id: 4,
+    name: 'Deputi Bidang Dukungan Kebijakan Pemerintahan dan Wawasan Kebangsaan',
+    employee: 31,
+    color:'#74b856'
+  },
+  {
+    id: 5,
+    name: 'Deputi Bidang Administrasi',
+    employee: 186,
+    color:'#2d9dd1'
+  },
+  {
+    id: 6,
+    name: 'Kementerian Sekretariat Negara',
+    employee: 15,
+    color:'#8a499e'
+  }
+]
