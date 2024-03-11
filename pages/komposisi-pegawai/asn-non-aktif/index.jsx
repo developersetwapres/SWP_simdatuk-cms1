760f2fd8521d4f00485f2f5657962b05f5560a4d
Ari Suharsa <ari@ekuator.id>
2024-03-12 00:46:15 +0700

    feat: asn non active page

---
+++ pages/komposisi-pegawai/asn-non-aktif/index.jsx
@@ -0,0 +1,12 @@
+import AsnNonActiveContainer from '@/containers/employee/asn-non-active/AsnNonActiveContainer'
+import React from 'react'
+
+const index = () => {
+  return (
+    <>
+      <AsnNonActiveContainer />
+    </>
+  )
+}
+
+export default index
