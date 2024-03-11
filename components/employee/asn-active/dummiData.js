2dbc0889ecf444a4762012076eeeba5f5868fbea
Ari Suharsa <ari@ekuator.id>
2024-03-12 00:57:01 +0700

    feat: dummy data

---
+++ components/employee/asn-active/dummiData.js
@@ -0,0 +1,296 @@
+export const pejabatPimpinan = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pimpinan Tinggi Madya (Eselon I)',
+    amount: 4
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pimpinan Tinggi Pratama (Eselon II)',
+    amount: 15
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Administrator (Eselon III)',
+    amount: 10
+  },
+  {
+    id: 4,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pengawas (Eselon IV)',
+    amount: 23
+  }
+]
+
+export const pejabatPelaksana = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pelaksana Golongan IV',
+    amount: 0
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pelaksana Golongan III',
+    amount: 47
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pelaksana Golongan II',
+    amount: 17
+  },
+  {
+    id: 4,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Pelaksana Perbantuan TNI dan POLRI',
+    amount: 31
+  }
+]
+
+export const pejabatFungsionalKeahlian = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Ahli Utama',
+    amount: 0
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Ahli Madya',
+    amount: 33
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Ahli Muda',
+    amount: 68
+  },
+  {
+    id: 4,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Ahli Pertama',
+    amount: 12
+  }
+]
+
+export const pejabatFungsionalKeterampilan = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Penyelia',
+    amount: 4
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Mahir',
+    amount: 0
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Terampil',
+    amount: 4
+  },
+  {
+    id: 4,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional Pemula',
+    amount: 0
+  }
+]
+
+export const pejabatKemensetneg = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Struktural',
+    amount: 2
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Pejabat Fungsional',
+    amount: 13
+  }
+]
+
+export const asnNonActive = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Tugas Belajar Luar Negeri (TBL)',
+    amount: 3
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Cuti Diluar Tanggungan Negara (CLTN)',
+    amount: 3
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Tidak Aktif (Non Jabatan)',
+    amount: 0
+  }
+]
+
+export const nonAsn = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Staf Khusus Wakil Presiden',
+    amount: 10
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Asisten Staf Khusus Wakil Presiden',
+    amount: 20
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pembantu Asisten Staf Khusus Wakil Presiden',
+    amount: 5
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Anggota Tim Ahli Wakil Presiden',
+    amount: 12
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Staf Pada Sekretaris Pribadi Istri Wakil Presiden',
+    amount: 1
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Staf Kerumahtanggaan Pada Kediaman Wakil Presiden',
+    amount: 1
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Sekretariat Pada Staf Khusus Wakil Presiden (PTT dari SETKAB)',
+    amount: 3
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Ajudan Wakil Presiden dan Istri Wakil Presiden (Perbantuan TNI dan POLRI)',
+    amount: 8
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Dokter Pribadi Wakil Presiden',
+    amount: 1
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Pengemudi VVIP (Perbantuan TNI dan POLRI)',
+    amount: 10
+  }
+]
+
+export const tnp2k = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'TNP2K',
+    amount: 64
+  }
+]
+
+export const tpps = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'TPPS',
+    amount: 24
+  }
+]
+
+export const tenagaOutsourcing = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Pengemudi',
+    amount: 38
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Petugas Kebersihan Gedung',
+    amount: 51
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Petugas Perawatan Kolam',
+    amount: 2
+  },
+  {
+    id: 4,
+    path: '/komposisi-pegawai/',
+    name: 'Petugas Taman',
+    amount: 24
+  },
+  {
+    id: 5,
+    path: '/komposisi-pegawai/',
+    name: 'Pramusaji/Pramubakti',
+    amount: 39
+  },
+  {
+    id: 6,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Jaringan',
+    amount: 2
+  },
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Komputer',
+    amount: 11
+  },
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Mekanikal dan Elektrikal',
+    amount: 24
+  }
+]
+
+export const nonOutsourcing = [
+  {
+    id: 1,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Fotocopy',
+    amount: 3
+  },
+  {
+    id: 2,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Road Blocker',
+    amount: 2
+  },
+  {
+    id: 3,
+    path: '/komposisi-pegawai/',
+    name: 'Teknisi Lift',
+    amount: 2
+  }
+]
