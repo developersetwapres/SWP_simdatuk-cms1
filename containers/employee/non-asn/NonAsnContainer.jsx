40f744a4e3dad87c33a8e9d64cf6d7861cd4c38d
Ari Suharsa <ari@ekuator.id>
2024-03-12 00:45:44 +0700

    feat(komposisi_pegawai): non asn page

---
+++ containers/employee/non-asn/NonAsnContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '@/components/core/Layout'
+import CourseSkeleton from '@/components/course/CourseSkeleton'
+import NonAsnComponent from '@/components/employee/non-asn/NonAsnComponent'
+
+const NonAsnContainer = () => {
+
+
+  const [willRender, setWillRender] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setWillRender(true)
+    }, 5000)
+  }, [])
+
+
+  return (
+    <Layout
+      willRender={willRender}
+    >
+      {
+        willRender === false
+          ? (
+            <CourseSkeleton />
+          ) : (
+            <NonAsnComponent />
+          )
+      }
+    </Layout>
+  )
+}
+
+export default NonAsnContainer
--- pages/komposisi-pegawai/non-asn/index.jsx
+++ pages/komposisi-pegawai/non-asn/index.jsx
@@ -1,6 +1,7 @@
 import NonAsnContainer from '@/containers/employee/non-asn/NonAsnContainer'
 import React from 'react'
 
+
 const index = () => {
   return (
     <>
