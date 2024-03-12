c14b786ade04f613edb2a04e81563b1e1e904c78
Ari Suharsa <ari@ekuator.id>
2024-03-12 16:10:57 +0700

    feat: lihat profile page

---
+++ pages/komposisi-pegawai/lihat-profile/index.jsx
@@ -0,0 +1,13 @@
+import LihatProfileContainer from '@/containers/employee/lihat-profile/LihatProfileContainer'
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <>
+      <LihatProfileContainer />
+    </>
+  )
+}
+
+export default index
