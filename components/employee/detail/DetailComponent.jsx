4c64deffe6bfc2a331b9542d58ecf6e24df0bc47
Ari Suharsa <ari@ekuator.id>
2024-03-12 17:49:17 +0700

    feat: detail pegawai component

---
+++ components/employee/detail/DetailComponent.jsx
@@ -0,0 +1,12 @@
+import DetailPegawaiLayout from '@/components/core/DetailPegawaiLayout'
+import React from 'react'
+
+const DetailComponent = () => {
+  return (
+    <>
+      <DetailPegawaiLayout />
+    </>
+  )
+}
+
+export default DetailComponent
