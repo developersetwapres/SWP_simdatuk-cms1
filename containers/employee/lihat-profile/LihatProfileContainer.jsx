19d1166dfd9da9ff077dcae391fe5becd9a94802
Ari Suharsa <ari@ekuator.id>
2024-03-12 16:10:39 +0700

    feat: lihat profile component

---
+++ containers/employee/lihat-profile/LihatProfileContainer.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '@/components/core/Layout'
+import CourseSkeleton from '@/components/course/CourseSkeleton'
+import LihatProfileComponent from '@/components/employee/lihat-profile/LihatProfileComponent'
+
+const LihatProfileContainer = () => {
+
+
+  const [willRender, setWillRender] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setWillRender(true)
+    }, 5000)
+  }, [])
+
+
+  return (
+    <Layout
+      willRender={willRender}
+    >
+      {
+        willRender === false
+          ? (
+            <CourseSkeleton />
+          ) : (
+            <LihatProfileComponent />
+          )
+      }
+    </Layout>
+  )
+}
+
+export default LihatProfileContainer
