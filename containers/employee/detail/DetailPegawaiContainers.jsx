7ce5a609a31b283207e964c85bdaae477ebf3f51
Ari Suharsa <ari@ekuator.id>
2024-03-12 17:49:45 +0700

    feat: detail pegawai container

---
+++ containers/employee/detail/DetailPegawaiContainers.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import Layout from '@/components/core/Layout'
+import CourseSkeleton from '@/components/course/CourseSkeleton'
+import DetailComponent from '@/components/employee/detail/DetailComponent'
+
+const DetailPegawaiContainers = () => {
+
+
+  const [willRender, setWillRender] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setWillRender(true)
+    }, 5000)
+  }, [])
+
+
+  return (
+    <Layout
+      willRender={willRender}
+    >
+      {
+        willRender === false
+          ? (
+            <CourseSkeleton />
+          ) : (
+            <DetailComponent />
+          )
+      }
+    </Layout>
+  )
+}
+
+export default DetailPegawaiContainers
