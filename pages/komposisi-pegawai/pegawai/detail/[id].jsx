2d0e4a450bd431b2cc8a0e6ea7c1fbfe1dd699f8
Ari Suharsa <ari@ekuator.id>
2024-03-12 17:51:58 +0700

    feat: detail pegawai page

---
+++ pages/komposisi-pegawai/pegawai/detail/[id].jsx
@@ -0,0 +1,12 @@
+import DetailPegawaiContainers from '@/containers/employee/detail/DetailPegawaiContainers'
+import React from 'react'
+
+const detail = () => {
+  return (
+    <>
+      <DetailPegawaiContainers />
+    </>
+  )
+}
+
+export default detail
