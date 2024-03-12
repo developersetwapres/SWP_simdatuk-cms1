b6712e37ae6fabfd1d9d4a91f160d707c3be9c34
Ari Suharsa <ari@ekuator.id>
2024-03-12 16:11:52 +0700

    feat: profile pegawai card

---
+++ components/core/card/ProfileCard.jsx
@@ -0,0 +1,166 @@
+import * as React from 'react'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import { Button } from '../../shared'
+import PropTypes from 'prop-types'
+import { Box, Grid } from '@mui/material'
+import Image from 'next/image'
+
+const style = {
+  cardParent: {
+    width: {
+      lg: '16vw',
+      md: '18vw',
+      sm: '25vw',
+      xs: '50vw'
+    },
+    height: '40rem'
+
+  },
+  imageBox: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    gap: '.5rem'
+  },
+  cardContent: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+}
+
+const ProfileCard = ({
+  rootStyle,
+  summary,
+  name,
+  eselon,
+  imageSource,
+  golongan,
+  nip
+}) => {
+
+
+  const handleButtonClick = () => {
+    console.log('ok')
+  }
+  return (
+    <Card sx={rootStyle || style.cardParent}>
+      <CardContent
+        sx={style.cardContent}
+      >
+        <Box
+          height='60px'
+        >
+          <Typography
+            sx={{ fontSize: 14 }}
+            fontWeight='bold'
+            gutterBottom
+            textAlign='center'
+
+          >
+            {summary}
+          </Typography>
+        </Box>
+        <Box
+          sx={style.imageBox}
+        >
+          {imageSource?.length > 0 && (
+            <Image
+              src={imageSource}
+              alt='profile'
+              height={200}
+              width={150}
+            />
+          )}
+          <Typography
+            color='primary'
+            fontWeight='600'
+            textAlign='center'
+          >
+            {name}
+          </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={1}
+        >
+          <Grid
+            item
+          >
+            <Typography fontSize={14}>
+              Eselon
+            </Typography>
+            <Typography
+              variant='p'
+              component='div'
+              fontSize={14}
+              fontWeight='600'
+            >
+              {eselon}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+          >
+            <Typography fontSize={14}>
+              Golongan
+            </Typography>
+            <Typography
+              variant='p'
+              component='div'
+              fontSize={14}
+              fontWeight='600'
+            >
+              {golongan}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+          >
+            <Typography
+              fontSize={14}
+            >
+              NIP/NRP
+            </Typography>
+            <Typography
+              variant='p'
+              component='div'
+              fontSize={14}
+              fontWeight='600'
+            >
+              {nip}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Button
+          onClick={handleButtonClick}
+          text='Lihat Detail'
+          color='sidatukDraweBase'
+          fullWidth
+          type='submit'
+          sx={{
+            color: '#fff',
+            textTransform: 'none'
+          }}
+        />
+      </CardContent>
+    </Card>
+  )
+}
+
+ProfileCard.propTypes = {
+  summary: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  imageSource: PropTypes.string,
+  eselon: PropTypes.string,
+  golongan: PropTypes.string,
+  nip: PropTypes.string,
+  rootStyle: PropTypes.object
+}
+
+export default ProfileCard
