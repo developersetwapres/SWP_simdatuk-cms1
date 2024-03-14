9895bc2ac5c398418a6b9674f1e0b3b882acb0e7
Ari Suharsa <ari@ekuator.id>
2024-03-14 20:31:04 +0700

    feat: detail pegawai component

--- components/employee/detail/DetailComponent.jsx
+++ components/employee/detail/DetailComponent.jsx
@@ -1,10 +1,206 @@
 import DetailPegawaiLayout from '@/components/core/DetailPegawaiLayout'
+import { Grid, Paper } from '@mui/material'
 import React from 'react'
+import RiwayatPendidikanSection from './section/RiwayatPendidikanSection'
+import BiodataPegawai from './section/BiodataPegawai'
+import RiwayatJabatanSection from './section/RiwayatJabatanSection'
+import RiwayatGolonganSection from './section/RiwayatGolongan'
+import RiwayatGajiSection from './section/RiwayatGajiSection'
+import PelatihanStrukturalSection from './section/PelatihanStrukturalSection'
+import PelatihanFungsionalSection from './section/PelatihanFungsional'
+import RiwayatPelatihanTeknisSection from './section/RiwayatPelatihanTeknisSection'
+import RiwayatPenghargaanSection from './section/RiwayatPenghargaanSection'
+import RiwayatSKP from './section/RiwayatSKPSection'
+import RiwayatPrestasiKerja from './section/RiwayatPrestasiKerja'
+import RiwayatHukumanDisiplin from './section/RiwayatHukumanDisiplin'
+import RiwayatKeluargaSection from './section/RiwayatKeluargaSection'
+import RiwayatCutiSection from './section/RiwayatCutiSection'
+import RiwayatCatatanSection from './section/RiwayatCatatanSection'
+
+
+
+
 
 const DetailComponent = () => {
+
+
   return (
     <>
-      <DetailPegawaiLayout />
+      <DetailPegawaiLayout
+      >
+        <Grid
+          container
+          xs={12}
+          width='100%'
+          borderRadius='12px'
+          paddingY={1}
+        >
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff'
+            }}
+          >
+            <BiodataPegawai />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatPendidikanSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatJabatanSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatGolonganSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatGajiSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <PelatihanStrukturalSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <PelatihanFungsionalSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatPelatihanTeknisSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatPenghargaanSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatSKP />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatPrestasiKerja />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatHukumanDisiplin />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatKeluargaSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatCutiSection />
+          </Paper>
+          <Paper
+            sx={{
+              width: '100%',
+              borderRadius: '12px',
+              padding: 2,
+              backgroundColor: '#fff',
+              marginTop: 2
+            }}
+          >
+            <RiwayatCatatanSection />
+          </Paper>
+        </Grid>
+      </DetailPegawaiLayout>
     </>
   )
 }
