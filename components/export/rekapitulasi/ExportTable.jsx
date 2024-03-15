68a6bdb714e394702908475e48a1fe5966e5fe60
Ari Suharsa <ari@ekuator.id>
2024-03-15 17:25:44 +0700

    feat: export table rekapitulasi

---
+++ components/export/rekapitulasi/ExportTable.jsx
@@ -0,0 +1,88 @@
+import { Box, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import PropsType from 'prop-types'
+
+
+const ExportTable = ({ summary, data }) => {
+
+  
+  const [total, setTotal] = useState('')
+
+  useEffect(() => {
+    if (summary) {
+      setTotal(data.reduce((acc, item) => acc + item.amount, 0))
+    }
+  }, [data, summary])
+
+
+  return (
+
+    <>
+      <Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'end'
+          }}
+        >
+          <Typography
+            fontWeight='500'
+            fontSize={14}
+            paddingTop={1}
+          >
+            {summary}
+          </Typography>
+          {
+            total && (
+              <Typography
+                fontWeight='500'
+                fontSize={14}
+              >
+                Total: {total}
+              </Typography>
+            )
+          }
+        </Box>
+        {
+          data.map((item, index) =>
+            <Box
+              key={index}
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                gap: '5px'
+              }}
+            >
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: 14
+                  }}
+                >
+                  {item.name}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: 14
+                  }}
+                >
+                  {item.amount}
+                </Typography>
+              </Box>
+            </Box>
+          )
+        }
+      </Box >
+    </>
+  )
+}
+
+ExportTable.propTypes = {
+  summary: PropsType.string,
+  data: PropsType.array
+}
+
+export default ExportTable
