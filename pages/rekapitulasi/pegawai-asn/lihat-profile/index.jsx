4dd7c2ba9b0e56b0ad6b39ea7f03a07234ef369d
Ari Suharsa <ari@ekuator.id>
2024-03-17 01:47:36 +0700

    feat(Pegawai ASN): lihat profile page

---
+++ pages/rekapitulasi/pegawai-asn/lihat-profile/index.jsx
@@ -0,0 +1,13 @@
+import LihatProfileContainer from '@/containers/pegawaiAsnContainer/lihat-profile/LihatProfileContainer'
+import React from 'react'
+
+
+const index = () => {
+  return (
+    <>
+      <LihatProfileContainer />
+    </>
+  )
+}
+
+export default index
