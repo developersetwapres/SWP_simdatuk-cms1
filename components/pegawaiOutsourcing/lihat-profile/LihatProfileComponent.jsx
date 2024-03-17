47a6feccb6fb7f41f9a886e8809b3299e9fcab94
Ari Suharsa <ari@ekuator.id>
2024-03-17 11:35:54 +0700

    feat: list pegawai outsourcing

---
+++ components/pegawaiOutsourcing/lihat-profile/LihatProfileComponent.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Box, Grid } from '@mui/material'
+import ProfileCard from '@/components/core/card/ProfileCard'
+import EmployeeLayout from '@/components/employee/EmployeeLayout'
+import { listPegawai } from '@/components/pegawaiAsnComponent/dummyData'
+
+
+const LihatProfileComponent = () => {
+
+
+  return (
+    <Box
+      sx={{
+        paddingTop: '20px'
+      }}
+    >
+      <EmployeeLayout
+        summary='Petugas Taman'
+        totalAmount={listPegawai.length}
+        showExpButton={false}
+      >
+        <Grid
+          container
+          alignItems='center'
+          justifyContent='space-between'
+          sx={{
+            gap: '2.3rem',
+            padding: 2
+          }}
+        >
+          {
+            listPegawai.map((item, index) =>
+              <Grid
+                item
+                key={index + 1}
+              >
+                <ProfileCard
+                  summary={item.position}
+                  imageSource={item.image}
+                  name={item.name}
+                  eselon={item.eselon}
+                  golongan={item.golongan}
+                  nip={item.NIP}
+                />
+              </Grid>
+            )
+          }
+        </Grid>
+      </EmployeeLayout>
+    </Box>
+  )
+}
+
+export default LihatProfileComponent
