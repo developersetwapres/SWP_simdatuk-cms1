632b8ece44b939bfc881f7ebbe9a25dda9d73280
Ari Suharsa <ari@ekuator.id>
2024-03-17 11:36:49 +0700

    feat: pegawai outsourcing container

---
+++ containers/PegawaiOutsourcing/PegawaiOutsourcingContainer.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import PegawaiOutsourcingComponent from '@/components/PegawaiOutsourcing/PegawaiOutsourcingComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class OutsourcingContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 5000)
+    }
+
+
+    render() {
+      return (
+        <Layout
+          willRender={this.state.willRender}
+        >
+          <PegawaiOutsourcingComponent
+            {...this.state}
+            {...this.props}
+          />
+        </Layout>
+      )
+    }
+  }
+)
