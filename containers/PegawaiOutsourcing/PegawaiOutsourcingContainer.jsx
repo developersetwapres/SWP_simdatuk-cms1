39e28bdf96e82c5a251aeb12361839e304258527
Ari Suharsa <ari@ekuator.id>
2024-03-18 16:06:55 +0700

    refactor: edit path component

--- containers/PegawaiOutsourcing/PegawaiOutsourcingContainer.jsx
+++ containers/PegawaiOutsourcing/PegawaiOutsourcingContainer.jsx
@@ -4,7 +4,7 @@ import { mapStateToProps } from '@/store/'
 import PropTypes from 'prop-types'
 import { mapActions } from '@/store/'
 import Layout from '@/components/core/Layout'
-import PegawaiOutsourcingComponent from '@/components/PegawaiOutsourcing/PegawaiOutsourcingComponent'
+import PegawaiOutsourcingComponent from '../../components/pegawaiOutsourcing/PegawaiOutsourcingComponent'
 
 export default connect(
   mapStateToProps('banner'),
