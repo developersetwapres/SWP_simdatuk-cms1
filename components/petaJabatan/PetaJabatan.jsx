d5c207b5a0935149a14a84507d9ac41ccfb0556b
Ari Suharsa <ari@ekuator.id>
2024-03-18 16:05:39 +0700

    feat: peta jabatan

--- components/petaJabatan/PetaJabatan.jsx
+++ components/petaJabatan/PetaJabatan.jsx
@@ -3,6 +3,7 @@ import EmployeeLayout from '../employee/EmployeeLayout'
 import { Box, Grid, Typography } from '@mui/material'
 import Image from 'next/image'
 import { Button } from '../shared'
+import ProfileCard from '../core/card/ProfileCard'
 
 const styles = {
   headerMap: {
@@ -28,6 +29,49 @@ const styles = {
 
 
 const PetaJabatanComponent = () => {
+
+
+  const listPegawai = [
+    {
+      id: 1,
+      position: 'Kepala Sekretariat Wakil Presiden',
+      name: 'Ahmad Erani Yustika, S.E., M.Sc., Ph.Dr',
+      image: '/simdatuk/imagePegawai.png',
+      eselon: 'Es. I.a., 25-01-2021',
+      golongan: 'Pembina Utama Madya (IV/d), 01-04-2017',
+      NIP: '197303221997021001'
+    },
+    {
+      id: 2,
+      position: 'Asisten Deputi Ekonomi dan Keuangan',
+      name: 'Dr. Ir. Suprayoga Hadi, M.S.P.',
+      image: '/simdatuk/imagePegawai.png',
+      eselon: 'Es. I.a., 25-01-2021',
+      golongan: 'Pembina Utama (IV/e), 01-04-2017',
+      NIP: '1965053019991031002'
+    },
+    {
+      id: 3,
+      position:
+        'Asisten Deputi Industri, Perdagangan, Pariwisata, dan Ekonomi Kreatif ',
+      name: 'Dr. Velix Vernando Wanggai S.IP., MPA',
+      image: '/simdatuk/imagePegawai.png',
+      eselon: 'Es. I.a, 23-08-2022',
+      golongan: 'Pembina Utama Muda (IV/c), 01-10-2019',
+      NIP: '197202161998031005'
+    },
+    {
+      id: 4,
+      position: 'Kepala Subbagian Dukungan Administrasi',
+      name: 'Sapto Harjono Wahjoe Sedjati, S.Sos., M.A.',
+      image: '/simdatuk/imagePegawai.png',
+      eselon: 'Es. I.a, 01-03-2023',
+      golongan: 'Pembina Utama (IV/e), 01-03-2023',
+      NIP: '180004061 / 197010271995031001'
+    }
+  ]
+
+
   return (
     <EmployeeLayout
       summary='Peta Jabatan'
@@ -38,11 +82,13 @@ const PetaJabatanComponent = () => {
         direction='column'
         justifyContent='center'
         alignItems='center'
+      // gap={5}
       >
         <Box
           width='40vw'
           borderRadius={3}
           sx={styles.headerMap}
+          marginBottom={4}
         >
           <Typography
             textAlign='center'
@@ -102,6 +148,7 @@ const PetaJabatanComponent = () => {
             <Grid
               item
               container
+              paddingY={2}
               justifyContent='space-between'
             >
               <Grid
@@ -145,8 +192,10 @@ const PetaJabatanComponent = () => {
                 <Typography
                   fontWeight='600'
                   fontSize={14}
-                  whiteSpace='pre-wrap'
-                  wordWrap='break-word'
+                  sx={{
+                    whiteSpace: 'pre-wrap',
+                    wordWrap: 'break-word'
+                  }}
                 >
                   197303221997021001
                 </Typography>
@@ -184,8 +233,63 @@ const PetaJabatanComponent = () => {
             </Grid>
           </Grid>
         </Grid>
+        <hr
+          style={{
+            height: '50px',
+            backgroundColor: '#394346',
+            width: '2px',
+            border: 0,
+            margin: '0 auto'
+          }}
+        />
+        <hr
+          style={{
+            width: '71.1%',
+            height: '2px',
+            border: 0,
+            margin: 0,
+            backgroundColor: '#394346'
+          }}
+        />
+        <Grid
+          container
+          item
+          gutter={0}
+          padding={0}
+          justifyContent='center'
+          gap={3}
+        >
+          {
+            listPegawai.map((item, index) =>
+              <Grid
+                item
+                key={index + 1}
 
+              >
+                <hr
+                  style={{
+                    height: '10%',
+                    width: '2px',
+                    border: 0,
+                    backgroundColor: '#394346',
+                    margin: '0 auto'
+                  }}
+                />
+                <ProfileCard
+                  summary={item.position}
+                  name={item.name}
+                  imageSource={item.image}
+                  eselon={item.eselon}
+                  golongan={item.golongan}
+                  nip={item.NIP}
+                  lihatProfile='Lihat Profile'
+                  lihatDetail='Lihat Detail'
+                />
 
+              </Grid>
+            )
+          }
+        </Grid>
       </Grid>
     </EmployeeLayout >
   )
