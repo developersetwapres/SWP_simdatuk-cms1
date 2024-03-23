7bfcad20be1734f256a12d2d9853917beeedd915
Ari Suharsa <ari@ekuator.id>
2024-03-23 14:15:01 +0700

    fix: fixing struktur line

--- components/petaJabatan/StrukturPetaJabatan.jsx
+++ components/petaJabatan/StrukturPetaJabatan.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ProfileCard from '../core/card/ProfileCard'
 import { Box, Grid, List, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
+import { HiArrowsExpand } from 'react-icons/hi'
 
 const styles = {
   jabatanFungsional: {
+    position: 'relative',
     borderRadius: '5px',
     backgroundColor: '#f6ebda',
     paddingY: 2,
     paddingX: 1,
+    marginX: 2,
     width: {
       lg: '16vw',
       md: '18vw',
@@ -22,10 +25,17 @@ const styles = {
 const StrukturPetaJabatan = ({
   data,
   styleBoxFungsional,
-  styleBoxProfile
+  styleBoxProfile,
+  openModal = () => { }
 }) => {
 
-  console.log(data.length)
+
+  const handleOpenModal = (item) => {
+    openModal(item, true)
+  }
+
+
+
   return (
     <>
       <hr
@@ -37,39 +47,12 @@ const StrukturPetaJabatan = ({
           margin: '0 auto'
         }}
       />
-      {
-        data.length == 2 && (
-          <hr
-            style={{
-              width: '42.4%',
-              height: '2px',
-              border: 0,
-              margin: 0,
-              backgroundColor: '#394346'
-            }}
-          />
-        )
-      }
-      {
-        data.length > 2 && (
-          <hr
-            style={{
-              width: '71.1%',
-              height: '2px',
-              border: 0,
-              margin: 0,
-              backgroundColor: '#394346'
-            }}
-          />
-        )
-      }
       <Grid
         container
         item
         gutter={0}
         padding={0}
         justifyContent='center'
-        gap={3}
         sx={{
           position: 'relative',
           width: '100%'
@@ -81,23 +64,95 @@ const StrukturPetaJabatan = ({
             <Grid
               item
               key={index + 1}
-
             >
+              {
+                item.id == '1' ? (
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'flex-end',
+                      padding: 0,
+                      margin: 0
+                    }}
+                  >
+                    <hr
+                      style={{
+                        width: '50.5%',
+                        height: '2px',
+                        border: 0,
+                        margin: 0,
+                        backgroundColor: '#394346'
+                      }}
+                    />
+                  </Box>
+                ) : item.id == data.length ? (
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'flex-start',
+                      padding: 0,
+                      margin: 0
+                    }}
+                  >
+                    <hr
+                      style={{
+                        width: '50.5%',
+                        height: '2px',
+                        border: 0,
+                        margin: 0,
+                        backgroundColor: '#394346'
+                      }}
+                    />
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      padding: 0,
+                      margin: 0,
+                      backgroundColor: '#394346'
+                    }}
+                  >
+                    <hr
+                      style={{
+                        width: '100%',
+                        height: '2px',
+                        border: 0,
+                        margin: 0,
+                        backgroundColor: '#394346'
+                      }}
+                    />
+                  </Box>
+                )
+              }
               {
                 item.jabatan === 'Jabatan Fungsional' ? (
                   <>
                     <hr
                       style={{
-                        height: '10%',
+                        height: '50px',
+                        backgroundColor: '#394346',
                         width: '2px',
                         border: 0,
-                        backgroundColor: '#394346',
                         margin: '0 auto'
                       }}
                     />
                     <Box
                       sx={styleBoxFungsional || styles.jabatanFungsional}
                     >
+                      <Box
+                        onClick={() => handleOpenModal(item)}
+                        position='absolute'
+                        right={15}
+                        top={10}
+                        fontSize={18}
+                        sx={{
+                          cursor: 'pointer'
+                        }}
+                      >
+                        <HiArrowsExpand />
+                      </Box>
                       <Typography
                         textAlign='center'
                         fontWeight='500'
@@ -139,39 +194,71 @@ const StrukturPetaJabatan = ({
                     </Box>
                   </>
                 ) : (
-                  <>
-                    <hr
-                      style={{
-                        height: '10%',
-                        width: '2px',
-                        border: 0,
-                        backgroundColor: '#394346',
-                        margin: '0 auto'
-                      }}
-                    />
-                    <ProfileCard
-                      rootStyle={styleBoxProfile}
-                      summary={item.position}
-                      name={item.name}
-                      imageSource={item.image}
-                      eselon={item.eselon}
-                      golongan={item.golongan}
-                      nip={item.NIP}
-                      lihatProfile='Lihat Profil'
-                      lihatDetail='Lihat Detail'
-                    /></>
+                  !item.name ? (
+                    <>
+                      <hr
+                        style={{
+                          height: '50px',
+                          width: '2px',
+                          border: 0,
+                          backgroundColor: '#394346',
+                          margin: '0 auto'
+                        }}
+                      />
+                      <Box
+                        sx={{
+                          padding: 2,
+
+                          backgroundColor: '#fff',
+                          width: '25vw'
+                        }}
+                      >
+                        <Typography
+                          textAlign={'center'}
+                          fontWeight='bold'
+                        >
+                          {item.position}
+                        </Typography>
+                      </Box>
+                    </>
+                  ) : (
+                    <>
+                      <hr
+                        style={{
+                          height: '50px',
+                          width: '2px',
+                          border: 0,
+                          backgroundColor: '#394346',
+                          margin: '0 auto'
+                        }}
+                      />
+                      <ProfileCard
+                        rootStyle={styleBoxProfile}
+                        summary={item.position}
+                        name={item.name}
+                        imageSource={item.image}
+                        eselon={item.eselon}
+                        golongan={item.golongan}
+                        nip={item.NIP}
+                        lihatProfile='Lihat Profil'
+                        lihatDetail='Lihat Detail'
+                      />
+                    </>
+                  )
                 )
               }
             </Grid>
           )
         }
-      </Grid>
+      </Grid >
     </>
   )
 }
 
+
 StrukturPetaJabatan.propTypes = {
   data: PropTypes.array,
+  openModal: PropTypes.func,
   styleBoxFungsional: PropTypes.object,
   styleBoxProfile: PropTypes.object
 }
