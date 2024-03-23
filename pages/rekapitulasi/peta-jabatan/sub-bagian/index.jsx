dcd5752f050b30f3fa80f4591aa01f80ba7e933d
Ari Suharsa <ari@ekuator.id>
2024-03-23 14:20:33 +0700

    feat: kepala sub bagian page

---
+++ pages/rekapitulasi/peta-jabatan/sub-bagian/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WithAuth from '@/components/shared/WithAuth'
+import KepalaSubBagianContainer from '@/containers/petaJabatanContainer/kepalaSubBagian/KepalaSubBagianContainer'
+
+const index = () => {
+  return (
+    <>
+      <KepalaSubBagianContainer />
+    </>
+  )
+}
+
+export default WithAuth(index)
+
