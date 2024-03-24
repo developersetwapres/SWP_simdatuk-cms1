18c4b4655f683e0bf55716fba1094b92025cc7ac
Ari Suharsa <ari@ekuator.id>
2024-03-24 19:36:47 +0700

    fix(script-src): fixing security policy

--- next.config.js
+++ next.config.js
@@ -1,45 +1,46 @@
-const path = require("path");
-const Dotenv = require("dotenv-webpack");
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 const alias = {
-  "@/components": path.join(__dirname, "components"),
-  "@/containers": path.join(__dirname, "containers"),
-  "@/hooks": path.join(__dirname, "hooks"),
-  "@/props": path.join(__dirname, "props"),
-  "@/utils": path.join(__dirname, "utils"),
-  "@/store": path.join(__dirname, "store"),
-};
+  '@/components': path.join(__dirname, 'components'),
+  '@/containers': path.join(__dirname, 'containers'),
+  '@/hooks': path.join(__dirname, 'hooks'),
+  '@/props': path.join(__dirname, 'props'),
+  '@/utils': path.join(__dirname, 'utils'),
+  '@/store': path.join(__dirname, 'store')
+}
 
 const ContentSecurityPolicy = `
   default-src 'self' https://simdatuk-api.ekuator.id/;
+  script-src 'self' 'unsafe-eval'; 
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   connect-src 'self' https://simdatuk-api.ekuator.id/;
   font-src 'self' https://fonts.gstatic.com;
   img-src 'self' https://simdatuk-api.ekuator.id/ data:;
   object-src 'none';
-`;
+`
 
 const securityHeaders = [
   {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
   },
   {
-    key: "X-Content-Type-Options",
-    value: "nosniff",
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
   },
   {
-    key: "Strict-Transport-Security",
-    value: "max-age=31536000; includeSubDomains; preload",
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
   },
   {
-    key: "Cache-Control",
-    value: "no-store, no-cache, must-revalidate",
+    key: 'Cache-Control',
+    value: 'no-store, no-cache, must-revalidate'
   },
   {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
-  },
-];
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
 
 const nextConfig = {
   reactStrictMode: true,
@@ -47,35 +48,35 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/auth/login",
-        permanent: false,
-      },
-    ];
+        source: '/',
+        destination: '/auth/login',
+        permanent: false
+      }
+    ]
   },
   async headers() {
     return [
       {
         // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: securityHeaders,
-      },
-    ];
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
   },
   webpack: (config) => {
-    config.plugins = config.plugins || [];
+    config.plugins = config.plugins || []
     config.plugins = [
       ...config.plugins,
       // Read the .env file
       new Dotenv({
-        path: path.join(__dirname, ".env.local"),
-        systemvars: true,
-      }),
-    ];
-    config.resolve.alias = Object.assign({}, config.resolve.alias, alias);
+        path: path.join(__dirname, '.env.local'),
+        systemvars: true
+      })
+    ]
+    config.resolve.alias = Object.assign({}, config.resolve.alias, alias)
 
-    return config;
-  },
-};
+    return config
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
