2bfcac6649337eea0eab94b3cf099e335be97153
Ari Suharsa <ari@ekuator.id>
2024-03-27 04:01:43 +0700

    feat: list pegawai component

---
+++ components/BandingkanPegawai/ListPegawai.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+import CheckboxCard from '../core/CheckboxCard'
+import { Box } from '@mui/material'
+import ProfileCard from '../core/card/ProfileCard'
+import { eselon1 } from '../Employee/AsnActive/dummiData'
+
+
+const ListPegawai = () => {
+  const [checkList, setCheckList] = useState([])
+  const [checkAll, setCheckAll] = useState(false)
+
+
+  const getCheckValue = (value) => {
+    if (checkList.length > 0) {
+      setCheckAll(false)
+      setCheckList([...checkList, value[0]])
+    } else {
+      setCheckAll(false)
+      setCheckList(value)
+    }
+  }
+
+  const deleteValueCheck = (id) => {
+    if (checkList.includes(id)) {
+      const index = checkList.indexOf(id)
+      if (index !== -1) {
+        const newArray = [...checkList]
+        newArray.splice(index, 1)
+        setCheckList(newArray)
+      }
+    }
+  }
+
+
+  const hanldeChekAll = () => {
+    setCheckAll(!checkAll)
+  }
+
+  console.log(checkList)
+
+  return (
+    <>
+      <CheckboxCard
+        checkedParent={checkAll}
+        checkIndeterminate={checkAll == false}
+        getChekAll={hanldeChekAll}
+        label='Pilih Semua'
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'row', ml: 3 }}>
+          {
+            eselon1.map((item, index) => {
+              return (
+                <ProfileCard
+                  name={item.name}
+                  eselon={item.eselon}
+                  golongan={item.golongan}
+                  nip={item.NIP}
+                  imageSource={item.image}
+                  check={true}
+                  key={index}
+                  id={item.id}
+                  chekValue={getCheckValue}
+                  deleteValue={deleteValueCheck}
+                  chekedAll={checkAll}
+                />
+              )
+            })
+          }
+        </Box>
+      </CheckboxCard>
+    </>
+  )
+}
+
+export default ListPegawai
