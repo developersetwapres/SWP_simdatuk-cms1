cc97ac564a8967edf7ed5111d5831e189595ed07
Ari Suharsa <ari@ekuator.id>
2024-03-29 04:43:09 +0700

    refactor: add button

--- components/Employee/EmployeeLayout.jsx
+++ components/Employee/EmployeeLayout.jsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react'
 import { Box, Typography } from '@mui/material'
 import ButtonExport from '../core/ButtonExport'
 import PropTypes from 'prop-types'
+import { Button } from '../shared'
 
 
-const EmployeeLayout = ({ children, summary, totalAmount, showExpButton }) => {
+const EmployeeLayout = ({ children, summary, totalAmount, showExpButton, reset, addBtn }) => {
 
   const [exportData, setExportData] = useState(showExpButton)
+  const [resetBtn, setResetBtn] = useState(reset)
+  const [addEmployee, setAddEmployee] = useState(addBtn)
+
 
 
   return (
@@ -19,6 +23,7 @@ const EmployeeLayout = ({ children, summary, totalAmount, showExpButton }) => {
             xs: 'column',
             md: 'row'
           },
+          alignItems: 'center',
           justifyContent: 'space-between',
           marginBottom: '20px',
           paddingX: '20px'
@@ -31,7 +36,13 @@ const EmployeeLayout = ({ children, summary, totalAmount, showExpButton }) => {
         >
           {summary}
         </Typography>
-        <Box>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 1,
+            marginTop: '20px'
+          }}
+        >
           {
             totalAmount && (
               <Typography
@@ -55,6 +66,34 @@ const EmployeeLayout = ({ children, summary, totalAmount, showExpButton }) => {
               <ButtonExport />
             </Box> : ''
           }
+          {resetBtn ?
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end'
+              }}
+            >
+              <Button
+                text='Reset Pegawai'
+                color='sidatukDraweBase'
+              />
+            </Box> : ''
+          }
+          {addEmployee ?
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'flex-end',
+                justifyContent: 'flex-end'
+              }}
+            >
+              <Button
+                text='Tambah Pegawai'
+                color='primary'
+              />
+            </Box> : ''
+          }
         </Box>
       </Box>
       {children}
@@ -66,7 +105,9 @@ EmployeeLayout.propTypes = {
   children: PropTypes.node.isRequired,
   summary: PropTypes.string.isRequired,
   totalAmount: PropTypes.number.isRequired,
-  showExpButton: PropTypes.bool.isRequired
+  showExpButton: PropTypes.bool.isRequired,
+  reset: PropTypes.bool,
+  addBtn: PropTypes.bool
 }
 
 export default EmployeeLayout
