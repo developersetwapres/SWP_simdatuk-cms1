e1436d4ed39917ff4b03a6d22fd2a5bb12b586e4
Ari Suharsa <ari@ekuator.id>
2024-04-01 01:41:52 +0700

    refactor: dummy data

--- components/BandingkanPegawai/DataPegawai/DummydataPegawai.js
+++ components/BandingkanPegawai/DataPegawai/DummydataPegawai.js
@@ -81,7 +81,7 @@ export const dummyDataPegawai = [
           'Petugas Penyiapan Jamuan, Subbagian Jamuan, Bagian Persidangan, Biro Protokol dan Persidangan, Deputi Seswapres Bidang Administrasi'
       }
     ],
-    riwayatPelatihanStruktural: [
+    pelatihanStruktural: [
       {
         id: 1,
         pelatihan: 'Diklat PIM Tk.IV'
@@ -91,7 +91,7 @@ export const dummyDataPegawai = [
         pelatihan: 'Prajabatan'
       }
     ],
-    pelatihanFungsional: '',
+    pelatihanFungsional: [],
     pelatihanTeknis: [
       {
         id: 1,
@@ -112,11 +112,10 @@ export const dummyDataPegawai = [
     riwayatCatatan: 'Sangat rajin dan selalu bisa diandalkan'
   },
   {
-    id: 1,
+    id: 2,
     image: '/simdatuk/imagePegawai.png',
-    name: 'Ade Ulfah Rahayu Ningsih, S.E.',
-    jabatan:
-      'Kepala Subbagian, Bagian Protokol, dan Kerumahtanggaan, Deputi Bidang Administrasi',
+    name: 'Ayu Pudianingtias, S.E., M.P.A.',
+    jabatan: 'Kepala Subbagian Dukungan Administrasi',
     eselon: 'Es. IV, 02-05-2018',
     golongan: 'Penata Tingkat I (III/d), 01-04-2023',
     nip: '1800005504 / 198806042010122004',
@@ -124,76 +123,37 @@ export const dummyDataPegawai = [
       {
         id: 1,
         jenjang: 'SD',
-        nama: 'SDN Karang Tengah 2',
+        nama: 'SDN 265 Jakarta',
         tahunLulus: '1991'
       },
       {
         id: 2,
         jenjang: 'SMP',
-        nama: 'SMP Negeri 1 Ciledug',
+        nama: 'SMP Negeri 12 Jakarta',
         tahunLulus: '1994'
       },
       {
         id: 3,
         jenjang: 'S1',
-        nama: 'SMU Negeri 90 Jakarta',
+        nama: 'SMU Negeri 70 Jakarta',
         tahunLulus: '1997'
       },
       {
         id: 4,
-        jenjang: 'D3',
+        jenjang: 'S1',
         universitas: 'Universitas Indonesia, 2000',
-        jurusan: 'Administrasi Perkantoran dan Sekretaris',
+        jurusan: 'Manajemen',
         programStudi: 'Administrasi Perkantoran dan Sekretaris',
         tahunLulus: '2000'
-      },
-      {
-        id: 5,
-        jenjang: 'S1',
-        universitas: 'Universitas Esa Unggul',
-        jurusan: 'Ekonomi Manajemen',
-        programStudi: 'Ekonomi Manajemen',
-        tahunLulus: '2006'
       }
     ],
     riwayatJabatan: [
       {
         id: 1,
-        jabatan:
-          'Kepala Subbagian Acara, Bagian Protokol, Biro Protokol, dan Kerumahtanggaan, Deputi Bidang Administrasi'
-      },
-      {
-        id: 2,
-        jabatan:
-          'Kepala Subbagian Pelayanan Protokol, Bagian Protokol, Biro Protokol, dan Kerumahtanggaan, Deputi Bidang Administrasi, Sekretariat Wakil Presiden'
-      },
-      {
-        id: 3,
-        jabatan:
-          'Kepala Subbagian Pelayanan Protokol, Bagian Acara dan Persidangan, Biro Protokol, Deputi Bidang Administrasi, Sekretariat Wakil Presiden'
-      },
-      {
-        id: 4,
-        jabatan:
-          'Kepala Subbagian Istana Wakil Presiden, Bagian Kerumahtanggaan, Biro Protokol, Deputi Bidang Administrasi, Sekretariat Wakil Presiden'
-      },
-      {
-        id: 5,
-        jabatan:
-          'Kepala Subbagian Istana Wakil Presiden, Bagian Kerumahtanggaan, Biro Protokol dan Kerumahtanggaan, Deputi Seswapres Bidang Administrasi'
-      },
-      {
-        id: 6,
-        jabatan:
-          'Penata Acara Kepresidenan pada Subbagian Acara, Bagian Acara dan Persidangan, Biro Protokol dan Kerumahtanggaan, Deputi Seswapres Bidang Administrasi'
-      },
-      {
-        id: 7,
-        jabatan:
-          'Petugas Penyiapan Jamuan, Subbagian Jamuan, Bagian Persidangan, Biro Protokol dan Persidangan, Deputi Seswapres Bidang Administrasi'
+        jabatan: 'Kepala Subbagian Dukungan Administrasi'
       }
     ],
-    riwayatPelatihanStruktural: [
+    pelatihanStruktural: [
       {
         id: 1,
         pelatihan: 'Diklat PIM Tk.IV'
@@ -203,24 +163,8 @@ export const dummyDataPegawai = [
         pelatihan: 'Prajabatan'
       }
     ],
-    pelatihanFungsional: '',
-    pelatihanTeknis: [
-      {
-        id: 1,
-        pelatihan:
-          'Webinar Setneg Serial Lecture 01 dengan tema Impacful Sociopreneursip'
-      },
-      {
-        id: 2,
-        pelatihan:
-          'Webinar Innovation Talks (InnoTalks) & Music “Building innovation Playground from A to Z”'
-      },
-      {
-        id: 3,
-        pelatihan:
-          'Kegiatan Seminar Virtual (Zoom Meeting) Program Pengembangan Kompentensi Biro Protokol dan Kerumahtanggaan oleh Ade Ulfah Rahayu Ningsih, S.E. dengan tema “Kendala dalam pelayanan Keprotokolan di Masa Kini'
-      }
-    ],
-    riwayatCatatan: 'Sangat rajin dan selalu bisa diandalkan'
+    pelatihanFungsional: [],
+    pelatihanTeknis: [],
+    riwayatCatatan: 'Pernah membuat kesalahan yang cukup fatal'
   }
 ]
