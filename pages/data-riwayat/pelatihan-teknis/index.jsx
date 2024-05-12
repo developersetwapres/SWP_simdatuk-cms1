cfd7f4c327d17ba055df118701a59229dcf1c673
azhar.rf <azhar@ekuator.id>
2024-05-12 22:35:14 +0700

    feat: riwayat pelatihan teknis pages

---
+++ pages/data-riwayat/pelatihan-teknis/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanTeknisContainer from '@/containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisContainer'
+
+const index = () => {
+  return <RiwayatPelatihanTeknisContainer />
+}
+
+export default index
