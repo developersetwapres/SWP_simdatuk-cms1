3a4b1928a29c80623c99d301ba9fdcb63630e38c
azhar.rf <azhar@ekuator.id>
2024-05-12 22:36:20 +0700

    feat: riwayat ppk pages

---
+++ containers/RiwayatContainer/PPKContainer/RiwayatPPKContainer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import RiwayatPPKComponent from '@/components/Riwayat/PPK/RiwayatPPKComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class RiwayatPPKContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      data: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 2000)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <RiwayatPPKComponent {...this.state} {...this.props} />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/data-riwayat/ppk/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPPKContainer from '@/containers/RiwayatContainer/PPKContainer/RiwayatPPKContainer'
+
+const index = () => {
+  return <RiwayatPPKContainer />
+}
+
+export default index
