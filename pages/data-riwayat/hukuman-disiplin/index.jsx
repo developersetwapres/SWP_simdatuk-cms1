a4aded4b6c591bd0cd109bfbcbf8c0988adf4bf2
azhar.rf <azhar@ekuator.id>
2024-05-12 22:36:40 +0700

    feat: riwayat hukuman disiplin pages

---
+++ pages/data-riwayat/hukuman-disiplin/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatHukumanDisiplinContainer from '@/containers/RiwayatContainer/HukumanDisiplinContainer/RiwayatHukumanDisiplinContainer'
+
+const index = () => {
+  return <RiwayatHukumanDisiplinContainer />
+}
+
+export default index
