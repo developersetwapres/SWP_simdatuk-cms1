39430ca12d135a9b98c8244ac3a21a4fa2db4655
azhar.rf <azhar@ekuator.id>
2024-05-12 22:34:47 +0700

    feat: riwayat pelatihan fungsional pages

---
+++ containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalContainer.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import RiwayatPelatihanFungsionalComponent from '@/components/Riwayat/Pelatihan/Fungsional/RiwayatPelatihanFungsionalComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class RiwayatPelatihanFungsionalContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      data: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 2000)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <RiwayatPelatihanFungsionalComponent
+            {...this.state}
+            {...this.props}
+          />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/data-riwayat/pelatihan-fungsional/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanFungsionalContainer from '@/containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalContainer'
+
+const index = () => {
+  return <RiwayatPelatihanFungsionalContainer />
+}
+
+export default index
