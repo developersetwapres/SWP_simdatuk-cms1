14d5444602335e89ee2d729a71aa86559d04583f
azhar.rf <azhar@ekuator.id>
2024-05-12 22:34:21 +0700

    feat: riwayat pelatihan struktural pages

---
+++ pages/data-riwayat/pelatihan-struktural/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanStrukturalContainer from '@/containers/RiwayatContainer/PelatihanContainer/StrukturalContainer/RiwayatPelatihanStrukturalContainer'
+
+const index = () => {
+  return <RiwayatPelatihanStrukturalContainer />
+}
+
+export default index
