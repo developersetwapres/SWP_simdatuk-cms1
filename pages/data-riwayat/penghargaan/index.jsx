65cc2db16383fb6557a358adee6f86f2aef685ad
azhar.rf <azhar@ekuator.id>
2024-05-12 22:35:35 +0700

    feat: riwayat penghargaan pages

---
+++ pages/data-riwayat/penghargaan/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPenghargaanContainer from '@/containers/RiwayatContainer/PenghargaanContainer/RiwayatPenghargaanContainer'
+
+const index = () => {
+  return <RiwayatPenghargaanContainer />
+}
+
+export default index
