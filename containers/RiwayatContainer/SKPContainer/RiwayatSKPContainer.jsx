33866fe387f2de70f9f13c018ef7caccbb613a35
azhar.rf <azhar@ekuator.id>
2024-05-12 22:35:57 +0700

    feat: riwayat skp pages

---
+++ containers/RiwayatContainer/SKPContainer/RiwayatSKPContainer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import RiwayatSKPComponent from '@/components/Riwayat/SKP/RiwayatSKPComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class RiwayatSKPContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      data: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 2000)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <RiwayatSKPComponent {...this.state} {...this.props} />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/data-riwayat/skp/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatSKPContainer from '@/containers/RiwayatContainer/SKPContainer/RiwayatSKPContainer'
+
+const index = () => {
+  return <RiwayatSKPContainer />
+}
+
+export default index
