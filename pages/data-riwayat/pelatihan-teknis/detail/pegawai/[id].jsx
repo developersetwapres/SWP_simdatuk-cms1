72addff2ed33ca847549d33c40f4db9033938bbe
azhar.rf <azhar@ekuator.id>
2024-05-17 16:18:13 +0700

    feat: detail pegawai pelatihan teknis pages

---
+++ pages/data-riwayat/pelatihan-teknis/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanTeknisDetailPegawaiContainer from '@/containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisDetailPegawaiContainer'
+
+const DetailPegawaiPelatihanTeknis = () => {
+  return <RiwayatPelatihanTeknisDetailPegawaiContainer />
+}
+
+export default DetailPegawaiPelatihanTeknis
