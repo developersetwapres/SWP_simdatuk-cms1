788705d1b79c393e6c2ce26c0e4986863eb50957
azhar.rf <azhar@ekuator.id>
2024-05-17 16:15:09 +0700

    feat: form edit & validation pelatihan fungsional pages

---
+++ pages/data-riwayat/pelatihan-fungsional/edit/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanFungsionalEditContainer from '@/containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalEditContainer'
+
+const RiwayatPelatihanFungsionalEdit = () => {
+  return <RiwayatPelatihanFungsionalEditContainer />
+}
+
+export default RiwayatPelatihanFungsionalEdit
