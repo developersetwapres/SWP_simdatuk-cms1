1bd85bc45656726ac8117a968e38bb63a4c0a8a3
azhar.rf <azhar@ekuator.id>
2024-05-17 16:16:09 +0700

    feat: detail pelatihan fungsional pages

---
+++ containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalDetailContainer.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import RiwayatPelatihanFungsionalDetailComponent from '@/components/Riwayat/Pelatihan/Fungsional/RiwayatPelatihanFungsionalDetailComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class RiwayatPelatihanFungsionalDetailContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      data: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 2000)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <RiwayatPelatihanFungsionalDetailComponent
+            {...this.state}
+            {...this.props}
+          />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/data-riwayat/pelatihan-fungsional/detail/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanFungsionalDetailContainer from '@/containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalDetailContainer'
+
+const DetailRiwayatPelatihanFungsional = () => {
+  return <RiwayatPelatihanFungsionalDetailContainer />
+}
+
+export default DetailRiwayatPelatihanFungsional
