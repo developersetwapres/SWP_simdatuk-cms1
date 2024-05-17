fc91d343cde91b1ed0ac0c5ea28fdce53455e97c
azhar.rf <azhar@ekuator.id>
2024-05-17 16:10:46 +0700

    feat: form add new & validation pelatihan struktural pages

---
+++ pages/data-riwayat/pelatihan-struktural/add.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanStrukturalAddContainer from '@/containers/RiwayatContainer/PelatihanContainer/StrukturalContainer/RiwayatPelatihanStrukturalAddContainer'
+
+const RiwayatPelatihanStrukturalAdd = () => {
+  return <RiwayatPelatihanStrukturalAddContainer />
+}
+
+export default RiwayatPelatihanStrukturalAdd
