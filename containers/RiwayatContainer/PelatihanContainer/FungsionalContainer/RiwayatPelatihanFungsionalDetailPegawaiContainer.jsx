5391041679ad33ae0a7fe540c87ec30bdb58fd38
azhar.rf <azhar@ekuator.id>
2024-05-17 16:16:34 +0700

    feat: detail pegawai pelatihan fungsional pages

---
+++ containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalDetailPegawaiContainer.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import EmployeeDetailComponent from '@/components/Employment/Employee/EmployeeDetailComponent'
+
+export default connect(
+  mapStateToProps('banner'),
+  mapActions('getBanners', 'deleteListBanner')
+)(
+  class RiwayatPelatihanFungsionalDetailPegawaiContainer extends Component {
+    static propTypes = {
+      banner: PropTypes.object,
+      data: PropTypes.object,
+      getBanners: PropTypes.func,
+      deleteListBanner: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+    }
+
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          willRender: true
+        })
+      }, 2000)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <EmployeeDetailComponent {...this.state} {...this.props} />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/data-riwayat/pelatihan-fungsional/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanFungsionalDetailPegawaiContainer from '@/containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalDetailPegawaiContainer'
+
+const DetailPegawaiPelatihanFungsional = () => {
+  return <RiwayatPelatihanFungsionalDetailPegawaiContainer />
+}
+
+export default DetailPegawaiPelatihanFungsional
