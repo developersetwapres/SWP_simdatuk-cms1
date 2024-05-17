b8e250f4dd992f528baa472a620671e9562ebaf8
azhar.rf <azhar@ekuator.id>
2024-05-17 16:14:53 +0700

    feat: form add new & validation pelatihan fungsional pages

---
+++ pages/data-riwayat/pelatihan-fungsional/add.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanFungsionalAddContainer from '@/containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalAddContainer'
+
+const RiwayatPelatihanFungsionalAdd = () => {
+  return <RiwayatPelatihanFungsionalAddContainer />
+}
+
+export default RiwayatPelatihanFungsionalAdd
