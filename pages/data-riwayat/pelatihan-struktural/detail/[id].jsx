1c2244601a6de1f5c908185092af8d1ee5baecfc
azhar.rf <azhar@ekuator.id>
2024-05-17 16:12:08 +0700

    feat: detail pelatihan struktural pages

---
+++ pages/data-riwayat/pelatihan-struktural/detail/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanStrukturalDetailContainer from '@/containers/RiwayatContainer/PelatihanContainer/StrukturalContainer/RiwayatPelatihanStrukturalDetailContainer'
+
+const DetailRiwayatPelatihanStruktural = () => {
+  return <RiwayatPelatihanStrukturalDetailContainer />
+}
+
+export default DetailRiwayatPelatihanStruktural
