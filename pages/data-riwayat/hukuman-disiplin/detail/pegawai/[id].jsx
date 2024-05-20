d77ac63f82f6ad05567cb45797c70fc7e455bfb8
azhar.rf <azhar@ekuator.id>
2024-05-20 17:21:13 +0700

    feat: detail pegawai riwayat hukdis pages

---
+++ pages/data-riwayat/hukuman-disiplin/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatHukumanDisiplinDetailPegawaiContainer from '@/containers/RiwayatContainer/HukumanDisiplinContainer/RiwayatHukumanDisiplinDetailPegawaiContainer'
+
+const DetailPegawaiHukumanDisiplin = () => {
+  return <RiwayatHukumanDisiplinDetailPegawaiContainer />
+}
+
+export default DetailPegawaiHukumanDisiplin
