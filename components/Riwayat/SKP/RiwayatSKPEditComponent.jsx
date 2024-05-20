dd8124e5d3d1d3b2391b4a09d6cab413f702d2c5
azhar.rf <azhar@ekuator.id>
2024-05-20 17:17:27 +0700

    feat: edit form & validation riwayat skp pages

---
+++ components/Riwayat/SKP/RiwayatSKPEditComponent.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable no-unused-vars */
+import React, { useRef } from 'react'
+import RiwayatJabatanForm from './RiwayatSKPForm'
+import { Formik } from 'formik'
+import LayoutPages from '@/components/core/LayoutPages'
+import { Box } from '@mui/material'
+import Card from '@/components/shared/Card/Index'
+import * as Yup from 'yup'
+import { Button } from '@/components/shared'
+import { useRouter } from 'next/router'
+
+const InitValue = {
+  namaSkp: '',
+  periodePenilaian: null,
+  periodePenilaianTahun: null,
+  periode: {
+    bulan: null,
+    tahun: null
+  },
+  pegawai: [
+    {
+      nama: null,
+      rating: null,
+      predikat: null,
+      pencapaian: null
+    }
+  ]
+}
+
+const FormSchema = Yup.object().shape({
+  namaPenghargaan: Yup.string().required('Nama Penghargaan tidak boleh kosong'),
+  periodePenilaian: Yup.string().required(
+    'Periode Penilaian tidak boleh kosong'
+  ),
+  periode: Yup.object().shape({
+    bulan: Yup.string().required('Bulan tidak boleh kosong'),
+    tahun: Yup.string().required('Tahun tidak boleh kosong')
+  }),
+  pegawai: Yup.array().of(
+    Yup.object().shape({
+      nama: Yup.string().required('Nama Pegawai tidak boleh kosong'),
+      rating: Yup.string().required('Rating Pegawai tidak boleh kosong'),
+      predikat: Yup.string().required('Predikat Pegawai tidak boleh kosong'),
+      pencapaian: Yup.string().required('Pencapaian Pegawai tidak boleh kosong')
+    })
+  )
+})
+
+const RiwayatSKPEditComponent = () => {
+  const router = useRouter()
+  const formikRef = useRef(null)
+
+  const handleSubmit = async (values) => {
+    try {
+      await FormSchema.validate(values, { abortEarly: false })
+
+      formikRef.current.setErrors({})
+    } catch (err) {
+      if (!err.inner || err.inner.length === 0) {
+        return
+      }
+
+      const newErrors = {}
+      err.inner.forEach((error) => {
+        newErrors[error.path] = error.message
+        formikRef.current.setFieldError(error.path, error.message)
+      })
+
+      const firstErrorField = err.inner[0].path
+      const firstErrorEl = document.querySelector(`[name="${firstErrorField}"]`)
+      firstErrorEl &&
+        firstErrorEl.scrollIntoView({ behavior: 'smooth', block: 'center' })
+    }
+  }
+
+  return (
+    <Formik
+      innerRef={formikRef}
+      initialValues={InitValue}
+      validationSchema={FormSchema}
+      onSubmit={() => {}}
+    >
+      {(formikProps) => (
+        <LayoutPages
+          summary={'Edit Riwayat SKP'}
+          handleBack={() => router.back()}
+          action={
+            <Box>
+              <Button
+                text='Simpan'
+                onClick={() => handleSubmit(formikProps?.values)}
+              />
+            </Box>
+          }
+        >
+          <Card>
+            <RiwayatJabatanForm formikRef={formikRef} {...formikProps} />
+          </Card>
+        </LayoutPages>
+      )}
+    </Formik>
+  )
+}
+
+export default RiwayatSKPEditComponent
---
+++ pages/data-riwayat/skp/edit/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatSKPEditContainer from '@/containers/RiwayatContainer/SKPContainer/RiwayatSKPEditContainer'
+
+const RiwayatSKPEdit = () => {
+  return <RiwayatSKPEditContainer />
+}
+
+export default RiwayatSKPEdit
