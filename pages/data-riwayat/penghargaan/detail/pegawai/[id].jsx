ed5a05ac0239e7c8b6001ddcac5320b8d448dc78
azhar.rf <azhar@ekuator.id>
2024-05-20 17:16:00 +0700

    feat: detail pegawai riwayat penghargaan pages

---
+++ pages/data-riwayat/penghargaan/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanTeknisDetailPegawaiContainer from '@/containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisDetailPegawaiContainer'
+
+const DetailPegawaiPenghargaan = () => {
+  return <RiwayatPelatihanTeknisDetailPegawaiContainer />
+}
+
+export default DetailPegawaiPenghargaan
