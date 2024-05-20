60e168637389f72f21c6a55966a326f1f8a12fbe
azhar.rf <azhar@ekuator.id>
2024-05-20 17:19:46 +0700

    feat: detail pegawai riwayat ppk pages

---
+++ pages/data-riwayat/ppk/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPPKDetailPegawaiContainer from '@/containers/RiwayatContainer/PPKContainer/RiwayatPPKDetailPegawaiContainer'
+
+const DetailPegawaiPPK = () => {
+  return <RiwayatPPKDetailPegawaiContainer />
+}
+
+export default DetailPegawaiPPK
