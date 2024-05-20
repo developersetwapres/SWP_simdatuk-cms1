42b959da5d5e9734b1c5ff222a595d7eb086d6ba
azhar.rf <azhar@ekuator.id>
2024-05-20 17:18:03 +0700

    feat: detail pegawai riwayat skp pages

---
+++ pages/data-riwayat/skp/detail/pegawai/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import RiwayatPelatihanTeknisDetailPegawaiContainer from '@/containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisDetailPegawaiContainer'
+
+const DetailPegawaiSKP = () => {
+  return <RiwayatPelatihanTeknisDetailPegawaiContainer />
+}
+
+export default DetailPegawaiSKP
