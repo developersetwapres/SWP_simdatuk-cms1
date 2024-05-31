30cca419d43eec93bd96c7dab50e1b24a7bf8560
azhar.rf <azhar@ekuator.id>
2024-05-31 11:08:43 +0700

    feat: master data edit & validation pages

---
+++ pages/master-data/role/edit/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MasterDataRoleEditContainer from '@/containers/MasterDataContainer/Role/MasterDataRoleEditContainer'
+
+const MasterDataRoleEdit = () => {
+  return <MasterDataRoleEditContainer />
+}
+
+export default MasterDataRoleEdit
