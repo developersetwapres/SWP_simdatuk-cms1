7b72f8e33b624239946feffce5ff4075f621d90e
azhar.rf <azhar@ekuator.id>
2024-05-31 11:09:00 +0700

    feat: master data detail pages

---
+++ containers/MasterDataContainer/Role/MasterDataRoleDetailContainer.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import MasterDataRoleDetailComponent from '@/components/MasterData/Role/MasterDataRoleDetailComponent'
+
+export default connect(
+  mapStateToProps('role'),
+  mapActions(
+    'getRole',
+    'deleteRole',
+    'getRolesOptions',
+    'getPermissions',
+    'clearRoleState'
+  )
+)(
+  class MasterDataRoleDetailContainer extends Component {
+    static propTypes = {
+      role: PropTypes.object,
+      getRole: PropTypes.func,
+      deleteRole: PropTypes.func,
+      getRolesOptions: PropTypes.func,
+      getPermissions: PropTypes.func,
+      clearRoleState: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
+        willRender: false
+      }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+      this.fetchGetRolesOptions = this.fetchGetRolesOptions.bind(this)
+    }
+
+    fetch() {
+      this.props.getPermissions()
+    }
+
+    fetchGetRolesOptions(queries) {
+      this.props.getRolesOptions(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
+    }
+
+    componentDidMount() {
+      this.fetch()
+      this.fetchGetRolesOptions(this.state.queries)
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <MasterDataRoleDetailComponent
+            onLoading={this.setLoading}
+            {...this.state}
+            {...this.props}
+          />
+        </Layout>
+      )
+    }
+  }
+)
---
+++ pages/master-data/role/detail/[id].jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MasterDataRoleDetailContainer from '@/containers/MasterDataContainer/Role/MasterDataRoleDetailContainer'
+
+const MasterDataRoleDetail = () => {
+  return <MasterDataRoleDetailContainer />
+}
+
+export default MasterDataRoleDetail
