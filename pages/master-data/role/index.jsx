0d479c3ba9fafb6341d3a2416dc5ab2911caba31
azhar.rf <azhar@ekuator.id>
2024-05-31 11:07:54 +0700

    feat: master data role pages

---
+++ pages/master-data/role/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MasterDataRoleContainer from '@/containers/MasterDataContainer/Role/MasterDataRoleContainer'
+
+const index = () => {
+  return <MasterDataRoleContainer />
+}
+
+export default index
