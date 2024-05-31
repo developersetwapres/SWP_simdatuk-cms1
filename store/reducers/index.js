a31ff697b6bbf47de29c9c59606b9ad044f61cd0
azhar.rf <azhar@ekuator.id>
2024-05-31 11:37:40 +0700

    fix: remove unused store

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,56 +1,22 @@
-import * as bannerReducer from './bannerReducer'
-import * as courseReducer from './course/courseReducer'
-import * as categoryReducer from './categoryReducer'
 import * as roleReducer from './users/roleReducer'
 import * as userReducer from './users/userReducer'
-import * as blacklistReducer from './users/blacklistReducer'
-import * as couponReducer from './coupon/couponReducer'
-import * as couponSubmissionReducer from './coupon/couponSubmissionReducer'
-import * as providerCourse from './course/providerCourseReducer'
 import * as authenticationReducer from './authenticationReducer'
 import * as modalReducer from './modalReducer'
-import * as commandReducer from './commandReducer'
-import * as providerReducer from './users/providerReducer'
-import * as activitylogReducer from './users/activitylogReducer'
-import * as menuReducer from './menuReducer'
-import * as editorReducer from './course/editorReducer'
 import * as employeeReducer from './employeeReducer'
-
-// * Excel
-import * as importExcelReducer from './excel/importExcelReducer'
-import * as exportExcelReducer from './excel/exportExcelReducer'
 
 // * Reducer
 import * as responserReducer from './responserReducer'
-
-// * Notification
-import * as notificationReducer from './notificationReducer'
 /**
  *
  * Root Reducers
  *
  */
 const reducers = {
-  ...bannerReducer,
-  ...courseReducer,
-  ...categoryReducer,
   ...roleReducer,
   ...userReducer,
-  ...blacklistReducer,
-  ...couponReducer,
-  ...couponSubmissionReducer,
-  ...providerCourse,
   ...authenticationReducer,
   ...modalReducer,
-  ...commandReducer,
-  ...providerReducer,
-  ...activitylogReducer,
-  ...menuReducer,
-  ...editorReducer,
-  ...importExcelReducer,
-  ...exportExcelReducer,
   ...responserReducer,
-  ...notificationReducer,
   ...employeeReducer
 }
 
--- store/sagas/index.js
+++ store/sagas/index.js
@@ -7,48 +7,9 @@ import { all } from '@redux-saga/core/effects'
 
 import authSaga from './authenticationSaga'
 import userSaga from './users/userSaga'
-import courseSaga from './course/courseSaga'
-import commandSaga from './commandSaga'
-import providerSaga from './users/providerSaga'
-import activitylogSaga from './users/activitylogSaga'
-import bannerSaga from './bannerSaga'
-import categorySaga from './categorySaga'
 import roleSaga from './users/roleSaga'
-import menuSaga from './menuSaga'
-import blacklistSaga from './users/blacklistSaga'
-import editorSaga from './course/editorSaga'
 import employeeSaga from './employeeSaga'
 
-// * Coupon
-import couponSaga from './coupon/couponSaga'
-import couponSubmissionSaga from './coupon/couponSubmissionSaga'
-
-// * Provider Course
-import providerCourseSaga from './course/providerCourseSaga'
-
-// * Excel
-import importExcelSaga from './excel/importExcelSaga'
-import exportExcelSaga from './excel/exportExcelSaga'
-
 export default function* rootSaga() {
-  yield all([
-    authSaga(),
-    userSaga(),
-    courseSaga(),
-    commandSaga(),
-    providerSaga(),
-    activitylogSaga(),
-    bannerSaga(),
-    categorySaga(),
-    roleSaga(),
-    menuSaga(),
-    blacklistSaga(),
-    editorSaga(),
-    couponSaga(),
-    couponSubmissionSaga(),
-    providerCourseSaga(),
-    importExcelSaga(),
-    exportExcelSaga(),
-    employeeSaga()
-  ])
+  yield all([authSaga(), userSaga(), roleSaga(), employeeSaga()])
 }
