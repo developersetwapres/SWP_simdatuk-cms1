e4a134f43f484881f6651d83c9c0a0337a8953b8
azhar.rf <azhar@ekuator.id>
2024-05-31 11:08:18 +0700

    feat: master data add new & form pages

---
+++ pages/master-data/role/add.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MasterDataRoleAddContainer from '@/containers/MasterDataContainer/Role/MasterDataRoleAddContainer'
+
+const MasterDataRoleAdd = () => {
+  return <MasterDataRoleAddContainer />
+}
+
+export default MasterDataRoleAdd
