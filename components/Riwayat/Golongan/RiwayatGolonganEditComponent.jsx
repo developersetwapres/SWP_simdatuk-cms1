9435fc0c12c6a25b0f34a6e4bfc76f2adec8b18d
azhar.rf <azhar@ekuator.id>
2024-06-05 16:56:21 +0700

    feat: intgration update grades

--- components/Riwayat/Golongan/RiwayatGolonganEditComponent.jsx
+++ components/Riwayat/Golongan/RiwayatGolonganEditComponent.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
+import PropTypes from 'prop-types'
 import { Formik } from 'formik'
 import LayoutPages from '@/components/core/LayoutPages'
 import { Box } from '@mui/material'
@@ -8,6 +10,8 @@ import * as Yup from 'yup'
 import { Button } from '@/components/shared'
 import { useRouter } from 'next/router'
 import RiwayatGolonganForm from './RiwayatGolonganForm'
+import moment from 'moment'
+import { monthsOptions } from 'libs/months'
 
 const InitValue = {
   namaGolongan: '',
@@ -70,19 +74,76 @@ const FormSchema = Yup.object().shape({
     })
 })
 
-const RiwayatGolonganEditComponent = () => {
+const RiwayatGolonganEditComponent = ({
+  grade,
+  employee,
+  getGrade = () => {},
+  updateGrade = () => {},
+  clearGradeState = () => {},
+  onLoading = () => {}
+}) => {
   const router = useRouter()
   const formikRef = useRef(null)
+
+  const options = useMemo(() => {
+    const newGrade = grade?.options?.map((itm) => itm?.name)
+    const newEmployee = employee?.data.map((itm) => {
+      return `${itm?.name} - ${itm?.employee_id_number}`
+    })
+
+    const data = {
+      month: monthsOptions || [],
+      golongan: newGrade || [],
+      employee: newEmployee || []
+    }
+
+    return data
+  }, [employee, grade])
+
+  const handleGetValueId = (val, type) => {
+    if (type == 'employee') {
+      const dataFilter = employee?.data.find(
+        (itm) => itm?.name == val.split(' - ')[0]
+      )
+      return dataFilter?.id
+    } else if (type == 'grade') {
+      const dataFilter = grade?.options.find((itm) => itm?.name == val)
+      return dataFilter?.id
+    } else {
+      const index = options['month'].findIndex((itm) => itm == val)
+      return index + 1
+    }
+  }
 
   const handleSubmit = async (values) => {
     try {
       await FormSchema.validate(values, { abortEarly: false })
-
       formikRef.current.setErrors({})
-    } catch (err) {
-      if (!err.inner || err.inner.length === 0) {
-        return
+
+      const id = atob(router?.query?.id)
+      const users = values?.pegawai.map((itm) => {
+        return {
+          id: handleGetValueId(itm?.nama, 'employee'),
+          user_id: handleGetValueId(itm?.nama, 'employee'),
+          grade_id: handleGetValueId(itm?.golongan, 'grade'),
+          effective_date: moment(itm?.tmt).format('YYYY-MM-DD'),
+          decree_name: itm?.noSk
+        }
+      })
+
+      const payload = {
+        id,
+        data: {
+          name: values?.namaGolongan,
+          period_month: handleGetValueId(values?.periode?.bulan, 'month'),
+          period_year: moment(values?.periode?.tahun).format('YYYY'),
+          users
+        }
       }
+
+      updateGrade(payload)
+    } catch (err) {
+      if (!err.inner || err.inner.length === 0) return
 
       const newErrors = {}
       err.inner.forEach((error) => {
@@ -98,8 +159,66 @@ const RiwayatGolonganEditComponent = () => {
   }
 
   useEffect(() => {
-    formikRef.current?.setFieldValue('namaGolongan', 'Golongan Terakhir', false)
+    // Get Detail User
+    const id = router?.query?.id
+    if (id) getGrade(atob(id))
+
+    // Event clear state when url path changes
+    router.events.on('routeChangeComplete', clearGradeState)
+
+    return () => {
+      router.events.off('routeChangeComplete', clearGradeState)
+    }
   }, [router])
+
+  useEffect(() => {
+    const state =
+      !grade?.loading &&
+      !employee?.loading &&
+      Object.entries(grade?.detail).length > 0
+    onLoading(state)
+  }, [grade, employee])
+
+  useEffect(() => {
+    const detail = grade?.detail
+
+    if (Object.entries(detail).length > 0) {
+      const newDates = new Date(detail?.period_year, detail?.period_month - 1)
+
+      formikRef.current?.setFieldValue('namaGolongan', detail?.name, false)
+      formikRef.current?.setFieldValue(
+        'periode.bulan',
+        monthsOptions[detail?.period_month - 1],
+        false
+      )
+      formikRef.current?.setFieldValue('periode.tahun', newDates, false)
+      detail?.users &&
+        detail?.users.map((itm, idx) => {
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].nama`,
+            itm?.name && itm?.employee_id_number
+              ? `${itm?.name} - ${itm?.employee_id_number}`
+              : null,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].golongan`,
+            itm?.grade_name,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].tmt`,
+            `${itm?.effective_date || ''}`,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].noSk`,
+            `${itm?.decree_number || ''}`,
+            false
+          )
+        })
+    }
+  }, [grade?.detail])
 
   return (
     <Formik
@@ -122,12 +241,25 @@ const RiwayatGolonganEditComponent = () => {
           }
         >
           <Card>
-            <RiwayatGolonganForm {...formikProps} formikRef={formikRef} />
+            <RiwayatGolonganForm
+              options={options}
+              formikRef={formikRef}
+              {...formikProps}
+            />
           </Card>
         </LayoutPages>
       )}
     </Formik>
   )
+}
+
+RiwayatGolonganEditComponent.propTypes = {
+  grade: PropTypes.object,
+  employee: PropTypes.object,
+  getGrade: PropTypes.func,
+  updateGrade: PropTypes.func,
+  clearGradeState: PropTypes.func,
+  onLoading: PropTypes.func
 }
 
 export default RiwayatGolonganEditComponent
--- containers/RiwayatContainer/GolonganContainer/RiwayatGolonganEditContainer.jsx
+++ containers/RiwayatContainer/GolonganContainer/RiwayatGolonganEditContainer.jsx
@@ -7,36 +7,62 @@ import Layout from '@/components/core/Layout'
 import RiwayatGolonganEditComponent from '@/components/Riwayat/Golongan/RiwayatGolonganEditComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('grade', 'employee'),
+  mapActions(
+    'getGrade',
+    'getGradesOptions',
+    'updateGrade',
+    'clearGradeState',
+    'getEmployees'
+  )
 )(
   class RiwayatGolonganEditContainer extends Component {
     static propTypes = {
-      banner: PropTypes.object,
-      data: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      grade: PropTypes.object,
+      getGradesOptions: PropTypes.func,
+      getGrade: PropTypes.func,
+      updateGrade: PropTypes.func,
+      clearGradeState: PropTypes.func,
+      getEmployees: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getGradesOptions(queries)
+      this.props.getEmployees(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <RiwayatGolonganEditComponent {...this.state} {...this.props} />
+          <RiwayatGolonganEditComponent
+            onLoading={this.setLoading}
+            {...this.state}
+            {...this.props}
+          />
         </Layout>
       )
     }
