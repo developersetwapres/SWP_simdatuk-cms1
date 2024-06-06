9657c3e4a5be6676f150ff482800c2e2b44b9068
azhar.rf <azhar@ekuator.id>
2024-06-06 19:10:47 +0700

    feat: integration training technics

--- containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisContainer.jsx
+++ containers/RiwayatContainer/PelatihanContainer/TeknisContainer/RiwayatPelatihanTeknisContainer.jsx
@@ -7,36 +7,98 @@ import Layout from '@/components/core/Layout'
 import RiwayatPelatihanTeknisComponent from '@/components/Riwayat/Pelatihan/Teknis/RiwayatPelatihanTeknisComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('training'),
+  mapActions('getTrainings')
 )(
   class RiwayatPelatihanTeknisContainer extends Component {
     static propTypes = {
-      banner: PropTypes.object,
-      data: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      training: PropTypes.object,
+      getTrainings: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10,
+          search: '',
+          type: 3
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.onPaginationChange = this.onPaginationChange.bind(this)
+      this.onRowsPerPageChange = this.onRowsPerPageChange.bind(this)
+      this.onSearch = this.onSearch.bind(this)
+      this.onClearState = this.onClearState.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getTrainings(queries)
+    }
+
+    onPaginationChange(page) {
+      const queries = {
+        ...this.state.queries,
+        page
+      }
+      this.setState({ queries })
+      this.fetch(queries)
+    }
+
+    onRowsPerPageChange(limit) {
+      const queries = {
+        ...this.state.queries,
+        page: 1,
+        limit
+      }
+      this.setState({ queries })
+      this.fetch(queries)
+    }
+
+    onSearch(value) {
+      const queries = {
+        ...this.state.queries,
+        search: value || '',
+        page: 1
+      }
+      this.setState({ queries })
+      this.fetch(queries)
+    }
+
+    onClearState() {
+      const queries = {
+        ...this.state.queries,
+        search: '',
+        page: 1
+      }
+      this.setState({ queries })
+      this.fetch(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <RiwayatPelatihanTeknisComponent {...this.state} {...this.props} />
+          <RiwayatPelatihanTeknisComponent
+            onSearch={this.onSearch}
+            onLoading={this.setLoading}
+            onPaginationChange={this.onPaginationChange}
+            onRowsPerPageChange={this.onRowsPerPageChange}
+            {...this.state}
+            {...this.props}
+          />
         </Layout>
       )
     }
