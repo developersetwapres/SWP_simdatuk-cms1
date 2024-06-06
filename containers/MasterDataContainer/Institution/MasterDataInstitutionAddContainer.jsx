2b10a62d259b09d6825cdc3828bee58674fef686
Difa Fadhil J <difa.fadhil.juliansyah@gmail.com>
2024-06-06 19:44:28 +0700

    feat: master data add new institution pages

---
+++ containers/MasterDataContainer/Institution/MasterDataInstitutionAddContainer.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { mapStateToProps } from '@/store/'
+import PropTypes from 'prop-types'
+import { mapActions } from '@/store/'
+import Layout from '@/components/core/Layout'
+import MasterDataInstitutionAddComponent from '@/components/MasterData/Institution/MasterDataInstitutionAddComponent'
+
+export default connect(
+  mapStateToProps('role'),
+  mapActions('getPermissions', 'postRole')
+)(
+  class MasterDataInstitutionAddContainer extends Component {
+    static propTypes = {
+      role: PropTypes.object,
+      getPermissions: PropTypes.func,
+      postRole: PropTypes.func
+    }
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        willRender: false
+      }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch() {
+      this.props.getPermissions()
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
+    }
+
+    componentDidMount() {
+      this.fetch()
+    }
+
+    render() {
+      return (
+        <Layout willRender={this.state.willRender}>
+          <MasterDataInstitutionAddComponent
+            onLoading={this.setLoading}
+            {...this.state}
+            {...this.props}
+          />
+        </Layout>
+      )
+    }
+  }
+)
