348209433efb70f1b58bafa4552fc39fcbafb00b
Difa Fadhil J <difa.fadhil.juliansyah@gmail.com>
2024-06-06 19:51:04 +0700

    fix: master data institution form remove undefined import

--- components/MasterData/Institution/MasterDataInstitutionForm.jsx
+++ components/MasterData/Institution/MasterDataInstitutionForm.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Typography } from '@mui/material'
 import { Form, Input } from '@/components/shared'
-import PermissionRoleCheckbox from './PermissionRoleCheckbox'
 
 const MasterDataInstitutionForm = ({
   dataPermissions,
