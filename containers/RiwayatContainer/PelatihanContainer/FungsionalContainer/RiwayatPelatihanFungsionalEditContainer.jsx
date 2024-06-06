47b74ba6ba2cc5c4959d6d0f2360a6effc60c92a
azhar.rf <azhar@ekuator.id>
2024-06-06 19:07:53 +0700

    feat: integration update training fungsional

--- containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalEditContainer.jsx
+++ containers/RiwayatContainer/PelatihanContainer/FungsionalContainer/RiwayatPelatihanFungsionalEditContainer.jsx
@@ -7,36 +7,57 @@ import Layout from '@/components/core/Layout'
 import RiwayatPelatihanFungsionalEditComponent from '@/components/Riwayat/Pelatihan/Fungsional/RiwayatPelatihanFungsionalEditComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('training', 'employee'),
+  mapActions(
+    'getTraining',
+    'updateTraining',
+    'clearTrainingState',
+    'getEmployees'
+  )
 )(
   class RiwayatPelatihanFungsionalEditContainer extends Component {
     static propTypes = {
-      banner: PropTypes.object,
-      data: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      traning: PropTypes.object,
+      employee: PropTypes.object,
+      getTraining: PropTypes.func,
+      updateTraining: PropTypes.func,
+      clearTrainingState: PropTypes.func,
+      getEmployees: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getEmployees(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
           <RiwayatPelatihanFungsionalEditComponent
+            onLoading={this.setLoading}
             {...this.state}
             {...this.props}
           />
