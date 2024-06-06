69177db0197c3c8caf3e93737606ddbbecd0a9d1
azhar.rf <azhar@ekuator.id>
2024-06-06 12:51:44 +0700

    feat: integration update recognition

--- components/Riwayat/Penghargaan/RiwayatPenghargaanEditComponent.jsx
+++ components/Riwayat/Penghargaan/RiwayatPenghargaanEditComponent.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
+import PropTypes from 'prop-types'
 import RiwayatJabatanForm from './RiwayatPenghargaanForm'
 import { Formik } from 'formik'
 import LayoutPages from '@/components/core/LayoutPages'
@@ -8,9 +10,18 @@ import Card from '@/components/shared/Card/Index'
 import * as Yup from 'yup'
 import { Button } from '@/components/shared'
 import { useRouter } from 'next/router'
+import { monthsOptions } from 'libs/months'
+import moment from 'moment'
 
 const InitValue = {
-  namaJabatan: '',
+  namaPenghargaan: '',
+  keteranganPenghargaan: '',
+  jenisSk: null,
+  tanggalSk: '',
+  noSkPenghargaan: '',
+  tahunSk: null,
+  instansi: '',
+  received: '',
   periode: {
     bulan: null,
     tahun: null
@@ -23,7 +34,12 @@ const InitValue = {
 }
 
 const FormSchema = Yup.object().shape({
-  namaJabatan: Yup.string().required('Nama Jabatan tidak boleh kosong'),
+  namaPenghargaan: Yup.string().required('Nama Penghargaan tidak boleh kosong'),
+  jenisSk: Yup.string().required('Jenis SK tidak boleh kosong'),
+  tanggalSk: Yup.string().required('Tanggal SK tidak boleh kosong'),
+  noSkPenghargaan: Yup.string().required(
+    'No SK Penghargaan tidak boleh kosong'
+  ),
   periode: Yup.object().shape({
     bulan: Yup.string().required('Bulan tidak boleh kosong'),
     tahun: Yup.string().required('Tahun tidak boleh kosong')
@@ -34,20 +50,83 @@ const FormSchema = Yup.object().shape({
     })
   )
 })
-
-const RiwayatPenghargaanEditComponent = () => {
+const RiwayatPenghargaanEditComponent = ({
+  recognition,
+  employee,
+  decree,
+  getRecognition = () => {},
+  updateRecognition = () => {},
+  clearRecognitionState = () => {},
+  onLoading = () => {}
+}) => {
   const router = useRouter()
   const formikRef = useRef(null)
+
+  const options = useMemo(() => {
+    const newEmployees = employee?.data.map((itm) => {
+      return `${itm?.name} - ${itm?.employee_id_number}`
+    })
+    const newDecree = decree?.data.map((itm) => itm?.name)
+
+    const data = {
+      employee: newEmployees || [],
+      decree: newDecree || [],
+      month: monthsOptions || []
+    }
+
+    return data
+  }, [employee, decree])
+
+  const handleGetValueId = (val, type) => {
+    if (type == 'employee') {
+      const dataFilter = employee?.data.find(
+        (itm) => itm?.name == val.split(' - ')[0]
+      )?.id
+
+      return dataFilter
+    } else if (type == 'month') {
+      const index = monthsOptions.findIndex((itm) => itm == val) + 1
+
+      return index
+    } else {
+      const dataFilter = decree?.data.find((itm) => itm?.name == val)?.id
+
+      return dataFilter
+    }
+  }
 
   const handleSubmit = async (values) => {
     try {
       await FormSchema.validate(values, { abortEarly: false })
-
       formikRef.current.setErrors({})
-    } catch (err) {
-      if (!err.inner || err.inner.length === 0) {
-        return
+
+      const id = atob(router?.query?.id)
+      const users = values?.pegawai.map((itm) => {
+        return {
+          user_id: handleGetValueId(itm?.nama, 'employee')
+        }
+      })
+
+      const payload = {
+        id,
+        data: {
+          name: values?.namaPenghargaan,
+          period_month: handleGetValueId(values?.periode?.bulan, 'month'),
+          period_year: moment(values?.periode?.tahun).format('YYYY'),
+          description: values?.keteranganPenghargaan,
+          type_of_decree: handleGetValueId(values?.jenisSk, 'decree'),
+          decree_date: moment(values?.tanggalSk).format('YYYY-MM-DD'),
+          decree_number: values?.noSkPenghargaan,
+          decree_year: moment(values?.tahunSk).format('YYYY'),
+          awarding_institution: values?.instansi,
+          date_of_receipt: moment(values?.received).format('YYYY-MM-DD'),
+          users
+        }
       }
+
+      updateRecognition(payload)
+    } catch (err) {
+      if (!err.inner || err.inner.length === 0) return
 
       const newErrors = {}
       err.inner.forEach((error) => {
@@ -61,6 +140,90 @@ const RiwayatPenghargaanEditComponent = () => {
         firstErrorEl.scrollIntoView({ behavior: 'smooth', block: 'center' })
     }
   }
+
+  const handleClearState = () => {
+    formikRef.current.resetForm()
+    clearRecognitionState()
+  }
+
+  useEffect(() => {
+    // Get Detail User
+    const id = router?.query?.id
+    if (id) getRecognition(atob(id))
+
+    // Event clear state when url path changes
+    router.events.on('routeChangeComplete', handleClearState)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleClearState)
+    }
+  }, [router])
+
+  useEffect(() => {
+    const state =
+      !recognition?.loading &&
+      !employee?.loading &&
+      !decree?.loading &&
+      Object.entries(recognition?.detail).length > 0
+    onLoading(state)
+  }, [recognition, employee, decree])
+
+  useEffect(() => {
+    const detail = recognition?.detail
+
+    if (detail) {
+      const periodYear =
+        detail?.period_year && detail?.period_month
+          ? new Date(detail?.period_year, detail?.period_month - 1)
+          : null
+      const periodMonth = monthsOptions[detail?.period_month - 1] || null
+      const decreeType =
+        decree?.data.find((itm) => itm?.id == detail?.type_of_decree)?.name ||
+        null
+      const decreeDate = detail?.decree_date
+        ? new Date(detail?.decree_date)
+        : ''
+      const decreeYear = detail?.decree_date
+        ? new Date(detail?.decree_year, 0, 1)
+        : null
+      const receivDate = detail?.date_of_receipt
+        ? new Date(detail?.date_of_receipt)
+        : ''
+
+      formikRef.current?.setFieldValue('namaPenghargaan', detail?.name, false)
+      formikRef.current?.setFieldValue('periode.bulan', periodMonth, false)
+      formikRef.current?.setFieldValue('periode.tahun', periodYear, false)
+      formikRef.current?.setFieldValue(
+        'keteranganPenghargaan',
+        detail?.description,
+        false
+      )
+      formikRef.current?.setFieldValue('jenisSk', decreeType, false)
+      formikRef.current?.setFieldValue('tanggalSk', decreeDate, false)
+      formikRef.current?.setFieldValue(
+        'noSkPenghargaan',
+        detail?.decree_number,
+        false
+      )
+      formikRef.current?.setFieldValue('tahunSk', decreeYear, false)
+      formikRef.current?.setFieldValue(
+        'instansi',
+        detail?.awarding_institution,
+        false
+      )
+      formikRef.current?.setFieldValue('received', receivDate, false)
+      detail?.users &&
+        detail?.users.map((itm, idx) => {
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].nama`,
+            itm?.name && itm?.employee_id_number
+              ? `${itm?.name} - ${itm?.employee_id_number}`
+              : null,
+            false
+          )
+        })
+    }
+  }, [recognition?.detail])
 
   return (
     <Formik
@@ -83,12 +246,26 @@ const RiwayatPenghargaanEditComponent = () => {
           }
         >
           <Card>
-            <RiwayatJabatanForm formikRef={formikRef} {...formikProps} />
+            <RiwayatJabatanForm
+              options={options}
+              formikRef={formikRef}
+              {...formikProps}
+            />
           </Card>
         </LayoutPages>
       )}
     </Formik>
   )
+}
+
+RiwayatPenghargaanEditComponent.propTypes = {
+  recognition: PropTypes.object,
+  employee: PropTypes.object,
+  decree: PropTypes.object,
+  getRecognition: PropTypes.func,
+  updateRecognition: PropTypes.func,
+  clearRecognitionState: PropTypes.func,
+  onLoading: PropTypes.func
 }
 
 export default RiwayatPenghargaanEditComponent
--- containers/RiwayatContainer/PenghargaanContainer/RiwayatPenghargaanEditContainer.jsx
+++ containers/RiwayatContainer/PenghargaanContainer/RiwayatPenghargaanEditContainer.jsx
@@ -7,36 +7,64 @@ import Layout from '@/components/core/Layout'
 import RiwayatPenghargaanEditComponent from '@/components/Riwayat/Penghargaan/RiwayatPenghargaanEditComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('recognition', 'employee', 'decree'),
+  mapActions(
+    'getRecognition',
+    'updateRecognition',
+    'getEmployees',
+    'getDecrees',
+    'clearRecognitionState'
+  )
 )(
   class RiwayatPenghargaanEditContainer extends Component {
     static propTypes = {
-      banner: PropTypes.object,
-      data: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      recognition: PropTypes.object,
+      employee: PropTypes.object,
+      decree: PropTypes.object,
+      getRecognition: PropTypes.func,
+      updateRecognition: PropTypes.func,
+      getEmployees: PropTypes.func,
+      getDecrees: PropTypes.func,
+      clearRecognitionState: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getEmployees(queries)
+      this.props.getDecrees(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <RiwayatPenghargaanEditComponent {...this.state} {...this.props} />
+          <RiwayatPenghargaanEditComponent
+            onLoading={this.setLoading}
+            {...this.state}
+            {...this.props}
+          />
         </Layout>
       )
     }
