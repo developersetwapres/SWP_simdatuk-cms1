9917c225f99ae82b6350f6885c191c9003bce2f0
azhar.rf <azhar@ekuator.id>
2024-06-06 19:09:36 +0700

    feat: integration update training struktural

--- components/Riwayat/Pelatihan/Struktural/RiwayatPelatihanStrukturalEditComponent.jsx
+++ components/Riwayat/Pelatihan/Struktural/RiwayatPelatihanStrukturalEditComponent.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
+import PropTypes from 'prop-types'
 import { Formik } from 'formik'
 import LayoutPages from '@/components/core/LayoutPages'
 import { Box } from '@mui/material'
@@ -8,6 +10,8 @@ import * as Yup from 'yup'
 import { Button } from '@/components/shared'
 import { useRouter } from 'next/router'
 import RiwayatPelatihanStrukturalForm from './RiwayatPelatihanStrukturalForm'
+import { monthsOptions } from 'libs/months'
+import moment from 'moment'
 
 const InitValue = {
   namaDiklat: '',
@@ -39,71 +43,108 @@ const FormSchema = Yup.object().shape({
   tanggalPelaksanaan: Yup.string().required(
     'Tanggal Pelaksanaan tidak boleh kosong'
   ),
-  pegawai: Yup.array()
-    .of(
-      Yup.object().shape({
-        nama: Yup.string().required('Nama Pegawai tidak boleh kosong'),
-        sertifikat: Yup.mixed()
-          .nullable()
-          .test('fileType', 'Format file harus PNG, JPG, atau PDF', (value) => {
-            if (!value) return true
-            const fileType = value && value.type
-            return (
-              fileType === 'image/png' ||
-              fileType === 'image/jpeg' ||
-              fileType === 'application/pdf'
-            )
-          })
-          .test(
-            'fileSize',
-            'Ukuran file tidak boleh lebih dari 2MB',
-            (value) => {
-              if (!value) return true
-              return value.size <= 2000000
-            }
-          )
-      })
-    )
-    .test('is-unique', 'Nama Pegawai harus unik', function (values) {
-      const names = new Map()
-      const duplicateNames = new Set()
-
-      values.forEach((pegawai, index) => {
-        const { nama } = pegawai
-        if (names.has(nama)) {
-          duplicateNames.add({ nama, index })
-        } else {
-          names.set(nama, index)
-        }
-      })
-
-      if (duplicateNames.size > 0) {
-        const errors = []
-        duplicateNames.forEach((item) => {
-          errors.push(
-            new Yup.ValidationError(
-              `Nama Pegawai tidak boleh sama`,
-              null,
-              `pegawai[${item.index}].nama`
-            )
+  pegawai: Yup.array().of(
+    Yup.object().shape({
+      nama: Yup.string().required('Nama Pegawai tidak boleh kosong'),
+      sertifikat: Yup.mixed()
+        .nullable()
+        .test('fileType', 'Format file harus PNG, JPG, atau PDF', (value) => {
+          if (!value || typeof value !== 'object') return true
+          const fileType = value && value.type
+          return (
+            fileType === 'image/png' ||
+            fileType === 'image/jpeg' ||
+            fileType === 'application/pdf'
           )
         })
-        throw new Yup.ValidationError(errors)
-      }
-
-      return true
+        .test('fileSize', 'Ukuran file tidak boleh lebih dari 2MB', (value) => {
+          if (!value || typeof value !== 'object') return true
+          return value.size <= 2000
+        })
     })
+  )
 })
 
-const RiwayatPelatihanStrukturalEditComponent = () => {
+const RiwayatPelatihanStrukturalEditComponent = ({
+  training,
+  employee,
+  getTraining = () => {},
+  updateTraining = () => {},
+  clearTrainingState = () => {},
+  onLoading = () => {}
+}) => {
   const router = useRouter()
   const formikRef = useRef(null)
+
+  const options = useMemo(() => {
+    const newEmployees = employee?.data.map((itm) => {
+      return `${itm?.name} - ${itm?.employee_id_number}`
+    })
+    const data = {
+      month: monthsOptions || [],
+      employee: newEmployees || []
+    }
+
+    return data
+  }, [employee])
+
+  const handleGetValue = (value, type) => {
+    if (type == 'employee') {
+      const data = training?.detail?.users
+      const dataFilter = data.find((itm) => itm?.name == value.split(' - ')[0])
+
+      return dataFilter
+    } else {
+      const index = monthsOptions.findIndex((itm) => itm == value) + 1
+
+      return index
+    }
+  }
 
   const handleSubmit = async (values) => {
     try {
       await FormSchema.validate(values, { abortEarly: false })
-
       formikRef.current.setErrors({})
+
+      const id = atob(router?.query?.id)
+      const formData = new FormData()
+
+      formData.append('name', values?.namaDiklat)
+      formData.append(
+        'period_month',
+        handleGetValue(values?.periode?.bulan, 'month')
+      )
+      formData.append(
+        'period_year',
+        moment(values?.periode?.tahun).format('YYYY')
+      )
+      formData.append('reference_number', values?.noSurat)
+      formData.append('level', values?.jenjang)
+      formData.append(
+        'start_date',
+        moment(values?.tanggalPelaksanaan).format('YYYY-MM-DD')
+      )
+      formData.append('duration', values?.durasi)
+      formData.append('organizer', values?.penyelenggara)
+      formData.append('link', values?.materi)
+      formData.append('type', 1)
+
+      values?.pegawai.map((item, index) => {
+        formData.append(
+          `users[${index}][id]`,
+          handleGetValue(item?.nama, 'employee')?.id
+        )
+        formData.append(
+          `users[${index}][user_id]`,
+          handleGetValue(item?.nama, 'employee')?.user_id
+        )
+        formData.append(
+          `users[${index}][certificate]`,
+          handleGetValue(item?.nama, 'employee')?.certificate
+        )
+      })
+
+      updateTraining({ id, data: formData })
     } catch (err) {
       if (!err.inner || err.inner.length === 0) {
         return
@@ -121,6 +162,84 @@ const RiwayatPelatihanStrukturalEditComponent = () => {
         firstErrorEl.scrollIntoView({ behavior: 'smooth', block: 'center' })
     }
   }
+
+  const handleClearState = () => {
+    formikRef.current.resetForm()
+    clearTrainingState()
+  }
+
+  useEffect(() => {
+    // Get Detail User
+    const id = router?.query?.id
+    if (id) getTraining(atob(id))
+
+    // Event clear state when url path changes
+    router.events.on('routeChangeComplete', handleClearState)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleClearState)
+    }
+  }, [router])
+
+  useEffect(() => {
+    const state =
+      !training?.loading &&
+      !employee?.loading &&
+      Object.entries(training?.detail).length > 0
+    onLoading(state)
+  }, [training, employee])
+
+  useEffect(() => {
+    const detail = training?.detail
+    if (detail) {
+      const periodYear =
+        detail?.period_year && detail?.period_month
+          ? new Date(detail?.period_year, detail?.period_month - 1)
+          : null
+      const periodMonth = monthsOptions[detail?.period_month - 1] || null
+      const startDate = detail?.start_date ? new Date(detail?.start_date) : ''
+
+      formikRef.current?.setFieldValue('namaDiklat', detail?.name, false)
+      formikRef.current?.setFieldValue('periode.bulan', periodMonth, false)
+      formikRef.current?.setFieldValue('periode.tahun', periodYear, false)
+      formikRef.current?.setFieldValue('tanggalPelaksanaan', startDate, false)
+      formikRef.current?.setFieldValue(
+        'noSurat',
+        detail?.reference_number,
+        false
+      )
+      formikRef.current?.setFieldValue('jenjang', detail?.level, false)
+      formikRef.current?.setFieldValue(
+        'penyelenggara',
+        detail?.organizer,
+        false
+      )
+      formikRef.current?.setFieldValue('durasi', detail?.duration, false)
+      formikRef.current?.setFieldValue('materi', detail?.link, false)
+
+      detail?.users &&
+        detail?.users.map((itm, idx) => {
+          const file = itm?.certificate
+          const fileSplit = file.split('/')
+          const fileName = fileSplit[fileSplit.length - 1]
+          const userName =
+            itm?.name && itm?.employee_id_number
+              ? `${itm?.name} - ${itm?.employee_id_number}`
+              : null
+
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].nama`,
+            userName,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].sertifikat`,
+            fileName,
+            false
+          )
+        })
+    }
+  }, [training?.detail])
 
   return (
     <Formik
@@ -144,6 +263,7 @@ const RiwayatPelatihanStrukturalEditComponent = () => {
         >
           <Card>
             <RiwayatPelatihanStrukturalForm
+              options={options}
               formikRef={formikRef}
               {...formikProps}
             />
@@ -152,6 +272,15 @@ const RiwayatPelatihanStrukturalEditComponent = () => {
       )}
     </Formik>
   )
+}
+
+RiwayatPelatihanStrukturalEditComponent.propTypes = {
+  training: PropTypes.object,
+  employee: PropTypes.object,
+  getTraining: PropTypes.func,
+  updateTraining: PropTypes.func,
+  clearTrainingState: PropTypes.func,
+  onLoading: PropTypes.func
 }
 
 export default RiwayatPelatihanStrukturalEditComponent
