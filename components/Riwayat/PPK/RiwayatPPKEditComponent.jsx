8ff68da7f733ff05b7562077ee44ac8735b90a81
azhar.rf <azhar@ekuator.id>
2024-06-07 14:27:29 +0700

    feat: integration update performance

--- components/Riwayat/PPK/RiwayatPPKEditComponent.jsx
+++ components/Riwayat/PPK/RiwayatPPKEditComponent.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React, { useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
+import PropTypes from 'prop-types'
 import RiwayatJabatanForm from './RiwayatPPKForm'
 import { Formik } from 'formik'
 import LayoutPages from '@/components/core/LayoutPages'
@@ -8,6 +10,8 @@ import Card from '@/components/shared/Card/Index'
 import * as Yup from 'yup'
 import { Button } from '@/components/shared'
 import { useRouter } from 'next/router'
+import { monthsOptions } from 'libs/months'
+import moment from 'moment'
 
 const InitValue = {
   namaPPK: '',
@@ -40,15 +44,80 @@ const FormSchema = Yup.object().shape({
   )
 })
 
-const RiwayatPPKEditComponent = () => {
+const RiwayatPPKEditComponent = ({
+  performance,
+  employee,
+  getPerformance = () => {},
+  updatePerformance = () => {},
+  clearPerformanceState = () => {},
+  onLoading = () => {}
+}) => {
   const router = useRouter()
   const formikRef = useRef(null)
+
+  const options = useMemo(() => {
+    const newEmployees = employee?.data.map((itm) => {
+      return `${itm?.name} - ${itm?.employee_id_number}`
+    })
+
+    const data = {
+      month: monthsOptions || [],
+      employee: newEmployees,
+      keterangan: ['Kurang', 'Sedang', 'Cukup', 'Baik', 'Sangat Baik']
+    }
+
+    return data
+  }, [employee])
+
+  const handleGetValue = (value, type) => {
+    if (type == 'employee') {
+      const data = employee?.data
+      const dataFilter = data.find(
+        (itm) => itm?.name == value.split(' - ')[0]
+      )?.id
+
+      return dataFilter
+    } else if (type == 'month') {
+      const index = monthsOptions.findIndex((itm) => itm == value) + 1
+
+      return index
+    } else {
+      const index = options['keterangan'].findIndex((itm) => itm == value) + 1
+
+      return index
+    }
+  }
 
   const handleSubmit = async (values) => {
     try {
       await FormSchema.validate(values, { abortEarly: false })
-
       formikRef.current.setErrors({})
+
+      const usersData = performance?.detail?.users
+
+      const id = atob(router?.query?.id)
+      const users = values?.pegawai.map((itm) => {
+        return {
+          id: usersData.find((item) => item?.name == itm?.nama.split(' - ')[0])
+            ?.id,
+          user_id: handleGetValue(itm?.nama, 'employee'),
+          work_performance_score: itm?.nilai,
+          description: handleGetValue(itm?.keterangan, 'keterangan')
+        }
+      })
+
+      const payload = {
+        id,
+        data: {
+          name: values?.namaPPK,
+          period_month: handleGetValue(values?.periode?.bulan, 'month'),
+          period_year: moment(values?.periode?.tahun).format('YYYY'),
+          performance_period: values?.periodePPK,
+          users
+        }
+      }
+
+      updatePerformance(payload)
     } catch (err) {
       if (!err.inner || err.inner.length === 0) {
         return
@@ -66,6 +135,75 @@ const RiwayatPPKEditComponent = () => {
         firstErrorEl.scrollIntoView({ behavior: 'smooth', block: 'center' })
     }
   }
+
+  const handleClearState = () => {
+    formikRef.current.resetForm()
+    clearPerformanceState()
+  }
+
+  useEffect(() => {
+    // Get Detail User
+    const id = router?.query?.id
+    if (id) getPerformance(atob(id))
+
+    // Event clear state when url path changes
+    router.events.on('routeChangeComplete', handleClearState)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleClearState)
+    }
+  }, [router])
+
+  useEffect(() => {
+    const state =
+      !performance?.loading &&
+      !employee?.loading &&
+      Object.entries(performance?.detail).length > 0
+    onLoading(state)
+  }, [performance, employee])
+
+  useEffect(() => {
+    const detail = performance?.detail
+
+    if (Object.entries(detail).length > 0) {
+      const periodYear = new Date(detail?.period_year, detail?.period_month - 1)
+      const year = new Date(detail?.year, 0, 1)
+
+      formikRef.current?.setFieldValue('namaPPK', detail?.name, false)
+      formikRef.current?.setFieldValue(
+        'periode.bulan',
+        options['month'][detail?.period_month - 1],
+        false
+      )
+      formikRef.current?.setFieldValue('periode.tahun', periodYear, false)
+      formikRef.current?.setFieldValue(
+        'periodePPK',
+        detail?.performance_period,
+        false
+      )
+
+      detail?.users &&
+        detail?.users.map((itm, idx) => {
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].nama`,
+            itm?.name && itm?.employee_id_number
+              ? `${itm?.name} - ${itm?.employee_id_number}`
+              : null,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].nilai`,
+            itm?.work_performance_score,
+            false
+          )
+          formikRef.current?.setFieldValue(
+            `pegawai[${idx}].keterangan`,
+            options['keterangan'][itm?.description - 1],
+            false
+          )
+        })
+    }
+  }, [performance?.detail])
 
   return (
     <Formik
@@ -88,12 +226,25 @@ const RiwayatPPKEditComponent = () => {
           }
         >
           <Card>
-            <RiwayatJabatanForm formikRef={formikRef} {...formikProps} />
+            <RiwayatJabatanForm
+              options={options}
+              formikRef={formikRef}
+              {...formikProps}
+            />
           </Card>
         </LayoutPages>
       )}
     </Formik>
   )
+}
+
+RiwayatPPKEditComponent.propTypes = {
+  performance: PropTypes.object,
+  employee: PropTypes.object,
+  getPerformance: PropTypes.func,
+  updatePerformance: PropTypes.func,
+  clearPerformanceState: PropTypes.func,
+  onLoading: PropTypes.func
 }
 
 export default RiwayatPPKEditComponent
--- containers/RiwayatContainer/PPKContainer/RiwayatPPKEditContainer.jsx
+++ containers/RiwayatContainer/PPKContainer/RiwayatPPKEditContainer.jsx
@@ -7,36 +7,60 @@ import Layout from '@/components/core/Layout'
 import RiwayatPPKEditComponent from '@/components/Riwayat/PPK/RiwayatPPKEditComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('performance', 'employee'),
+  mapActions(
+    'getPerformance',
+    'updatePerformance',
+    'clearPerformanceState',
+    'getEmployees'
+  )
 )(
   class RiwayatPPKEditContainer extends Component {
     static propTypes = {
-      banner: PropTypes.object,
-      data: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      performance: PropTypes.object,
+      employees: PropTypes.object,
+      getPerformance: PropTypes.func,
+      updatePerformance: PropTypes.func,
+      clearPerformanceState: PropTypes.func,
+      getEmployees: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getEmployees(queries)
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <RiwayatPPKEditComponent {...this.state} {...this.props} />
+          <RiwayatPPKEditComponent
+            onLoading={this.setLoading}
+            {...this.state}
+            {...this.props}
+          />
         </Layout>
       )
     }
