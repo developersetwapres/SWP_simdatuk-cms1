2c5bd1ddf76ac08bb3557be01f54cb4dd5f97151
Difa Fadhil J <difa.fadhil.juliansyah@gmail.com>
2024-06-10 16:31:07 +0700

    feat: employee data functional training form

---
+++ components/DataPegawai/Form/FunctionalTrainingForm.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import { Button, Input, Autocomplete } from '@/components/shared'
+import { Box, Typography, Divider, Grid } from '@mui/material'
+import DatePickerDay from '@/components/shared/form/DatePickerDay'
+import UploadFile from '@/components/shared/form/UploadFile'
+import DatepickerYear from '@/components/shared/form/DatepickerYear'
+
+function FunctionalTrainingForm() {
+  return (
+    <>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Typography color='#895700' fontWeight={700}>Pelatihan Fungsional</Typography>
+
+        <Button
+          text='Hapus'
+          color='danger'
+          onClick={() => { }}
+          sx={{ textTransform: 'none' }}
+        />
+      </Box>
+
+      <Divider sx={{ border: '1px solid #929292', margin: '10px 0px' }} />
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography
+            sx={{
+              marginBottom: '8px',
+              fontSize: '14px',
+              fontWeight: 500
+            }}
+          >
+            Periode Input Riwayat *
+          </Typography>
+          <Grid container spacing={2}>
+            {/* Bulan */}
+            <Grid item xs={6}>
+              <Autocomplete
+                options={['a']}
+                name='periode.bulan'
+                placeholder='Pilih Bulan'
+                multiple={false}
+                value={''}
+                onChange={(val) => {
+                  console.log(val)
+                }}
+                error={''}
+              />
+            </Grid>
+            {/* Tahun */}
+            <Grid item xs={6}>
+              <DatepickerYear
+                isClear
+                name='periode.tahun'
+                placeholder='Pilih Tahun'
+                value={''}
+                error={''}
+                onChange={(val) => {
+                  console.log(val)
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={6}>
+          <Input
+            label='Nama Diklat *'
+            placeholder='Masukkan Nama Diklat'
+            name='name'
+            value={''}
+            error={''}
+            onChange={(val) => console.log(val)}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Input
+            label='No. Surat Perintah *'
+            placeholder='Masukkan No. Surat Perintah'
+            name='name'
+            value={''}
+            error={''}
+            onChange={(val) => console.log(val)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Input
+            label='Jenjang'
+            placeholder='Masukkan Jenjang'
+            name='name'
+            value={''}
+            error={''}
+            onChange={(val) => console.log(val)}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <DatePickerDay
+            value={''}
+            name={'dateOfBirth'}
+            label='Tanggal Pelaksanaan *'
+            placeholder='dd-mm-yyyy'
+            error={''}
+            onChange={(val) => {
+              console.log(val)
+            }}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Input
+            label='Durasi Pelatihan(Hari)'
+            placeholder='Masukkan Durasi Pelatihan(Hari)'
+            name='name'
+            value={''}
+            error={''}
+            onChange={(val) => console.log(val)}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <Input
+            label='Penyelenggara'
+            placeholder='Masukkan Penyelenggara'
+            name='name'
+            value={''}
+            error={''}
+            onChange={(val) => console.log(val)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <UploadFile
+            label='Sertifikat'
+            maxSize={2}
+            dataUnit='MB'
+            formatFile={['.png', '.jpg', '.pdf']}
+            name={'name'}
+            value={''}
+            error={''}
+            onDelete={() => { }}
+            onChange={(val) => {
+              console.log(val)
+            }}
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export default FunctionalTrainingForm
