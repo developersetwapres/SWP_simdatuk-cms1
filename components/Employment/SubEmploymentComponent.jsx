a95586c432be82bc8272f3f592d52923eadee8c8
Difa Fadhil J <difa.fadhil.juliansyah@gmail.com>
2024-06-16 21:53:12 +0700

    feat(sub-employment): implement recap apis

--- components/Employment/SubEmploymentComponent.jsx
+++ components/Employment/SubEmploymentComponent.jsx
@@ -1,133 +1,198 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable indent */
-import React, { useMemo } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Grid, Typography } from '@mui/material'
 import CardEmployment from '../shared/Card/CardEmployment'
 import { useRouter } from 'next/router'
 import Card from '../shared/Card/Index'
 import LayoutPages from '../core/LayoutPages'
+import { v4 as uuidv4 } from 'uuid'
 
-const SubEmploymentComponent = (props) => {
-  const { data } = props
+const SubEmploymentComponent = ({
+  recapData,
+  recapComposition,
+  recapASN,
+  recapNonASN,
+  recapOutsource,
+  getRecapData = () => { },
+  setRecapData = () => { },
+  setRender = () => { }
+}) => {
   const router = useRouter()
+  // Path
+  useEffect(() => {
+    console.log('ROUTER: ', router)
+    getRecapData(router)
+  }, [router])
 
-  const handleGetCountEmployee = (datas) => {
-    console.log('datas', datas)
-    return datas.reduce((accumulator, currentValue) => {
-      return accumulator + currentValue.count
-    }, 0)
-  }
+  // Loading
+  useEffect(() => {
+    setRender(!recapComposition?.loading)
+  }, [recapComposition?.loading])
 
-  // checking data length, if more than 1 return true
-  const dataLength = useMemo(() => {
-    return data?.children.length > 1 ? true : false
-  }, [data])
+  useEffect(() => {
+    setRender(!recapASN?.loading)
+  }, [recapASN?.loading])
 
-  // Retrieval of total employee data
-  const totalEmployee = useMemo(() => {
-    return data?.children.reduce((accumulator, currentValue) => {
-      currentValue.children.forEach((child) => {
-        accumulator += child.count
-      })
-      return accumulator
-    }, 0)
-  }, [data])
+  useEffect(() => {
+    setRender(!recapNonASN?.loading)
+  }, [recapNonASN?.loading])
+
+  useEffect(() => {
+    setRender(!recapOutsource?.loading)
+  }, [recapOutsource?.loading])
+
+  // Composition
+  useEffect(() => {
+    setRecapData(recapComposition?.data)
+  }, [recapComposition?.data])
+
+  // ASN
+  useEffect(() => {
+    setRecapData(recapASN?.data)
+  }, [recapASN?.data])
+
+  // NON-ASN
+  useEffect(() => {
+    setRecapData(recapNonASN?.data)
+  }, [recapNonASN?.data])
+
+  // OUTSOURCE
+  useEffect(() => {
+    setRecapData(recapOutsource?.data)
+  }, [recapOutsource?.data])
+
+  // Recap Data
+  useEffect(() => {
+    console.log('RECAP: ', recapData)
+  }, [recapData])
 
   return (
     <LayoutPages
       handleBack={() => router.back()}
-      summary={data?.title}
-      count={`Total Keseluruhan : ${totalEmployee}`}
+      summary={recapData?.name}
+      count={`Total Keseluruhan : ${recapData?.total}`}
     >
       <Grid container spacing={3}>
-        {data?.children.map((item, index) => (
-          <Grid item xs={12} key={index}>
-            <Card
-              otherStyle={{
-                padding: dataLength ? '20px' : '4px 0',
-                backgroundColor: dataLength ? '#FFF' : 'transparent',
-                boxShadow: dataLength
-                  ? '0px 4px 10px rgba(0, 0, 0, 0.1)'
-                  : 'none'
-              }}
-            >
-              <Box
-                sx={{
-                  width: '100%',
-                  display: dataLength ? 'flex' : 'none',
-                  alignItems: 'start',
-                  justifyContent: 'space-between'
-                }}
-              >
-                <Typography
-                  variant='h3'
-                  component='h3'
-                  color='primary'
-                  sx={{
-                    marginBottom: '12px',
-                    fontSize: '16px',
-                    fontWeight: 800
-                  }}
-                >
-                  {item?.title || '-'}
-                </Typography>
-                <Typography
-                  variant='h3'
-                  component='h3'
-                  color='primary'
-                  sx={{
-                    marginBottom: '12px',
-                    fontSize: '16px',
-                    fontWeight: 800
-                  }}
-                >
-                  {handleGetCountEmployee(item?.children)}
-                </Typography>
-              </Box>
-              <Grid
-                container
-                spacing={3}
-                alignItems='start'
-                justifyContent='space-between'
-              >
-                {item?.children.map((itm, idx) => (
-                  <Grid
-                    item
-                    key={idx}
-                    xs={6}
-                    sm={
-                      item?.children.length == 2
-                        ? 6
-                        : item?.children.length == 3
-                        ? 4
-                        : 3
-                    }
-                  >
-                    <CardEmployment
-                      data={itm}
-                      path={`/rekapitulasi/${router?.query?.employment}/${btoa(
-                        itm?.category
-                      )}/pegawai`}
-                      cardStyle={{
-                        border: dataLength ? '2px solid #394346' : 'none',
-                        boxShadow: !dataLength
-                          ? '0px 4px 10px rgba(0, 0, 0, 0.1)'
-                          : 'none'
-                      }}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-            </Card>
-          </Grid>
+        {recapData?.cards?.map((item) => (
+          <RecapItem
+            key={uuidv4()}
+            showBackground={recapData?.cards?.length > 1}
+            count={item?.total}
+            cards={item?.cards?.map(i => ({ title: i?.name, count: i?.total }))}
+            title={item?.name}
+          />
         ))}
       </Grid>
     </LayoutPages>
   )
 }
 
+const RecapItem = ({
+  showBackground,
+  title,
+  count,
+  cards
+}) => {
+  const router = useRouter()
+  return (
+    <Grid item xs={12}>
+      <Card
+        otherStyle={{
+          padding: showBackground ? '20px' : '4px 0',
+          backgroundColor: showBackground ? '#FFF' : 'transparent',
+          boxShadow: showBackground
+            ? '0px 4px 10px rgba(0, 0, 0, 0.1)'
+            : 'none'
+        }}
+      >
+        <Box
+          sx={{
+            width: '100%',
+            display: showBackground ? 'flex' : 'none',
+            alignItems: 'start',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Typography
+            variant='h3'
+            component='h3'
+            color='primary'
+            sx={{
+              marginBottom: '12px',
+              fontSize: '16px',
+              fontWeight: 800
+            }}
+          >
+            {title || '-'}
+          </Typography>
+          <Typography
+            variant='h3'
+            component='h3'
+            color='primary'
+            sx={{
+              marginBottom: '12px',
+              fontSize: '16px',
+              fontWeight: 800
+            }}
+          >
+            {count}
+          </Typography>
+        </Box>
+        <Grid
+          container
+          spacing={3}
+          alignItems='start'
+          justifyContent='space-between'
+        >
+          {cards?.map((itm, idx) => (
+            <Grid
+              item
+              key={idx}
+              xs={6}
+              sm={
+                cards.length == 2
+                  ? 6
+                  : cards.length == 3
+                    ? 4
+                    : 3
+              }
+            >
+              <CardEmployment
+                data={itm}
+                // path={`/rekapitulasi/${router?.query?.employment}/${btoa(idx + 1)}/pegawai`}
+                path={router.asPath}
+                cardStyle={{
+                  border: showBackground ? '2px solid #394346' : 'none',
+                  boxShadow: !showBackground
+                    ? '0px 4px 10px rgba(0, 0, 0, 0.1)'
+                    : 'none'
+                }}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Card>
+    </Grid>
+  )
+}
+
 SubEmploymentComponent.propTypes = {
-  data: PropTypes.array
+  recapData: PropTypes.object,
+  recapComposition: PropTypes.object,
+  recapASN: PropTypes.object,
+  recapNonASN: PropTypes.object,
+  recapOutsource: PropTypes.object,
+  getRecapData: PropTypes.func,
+  setRecapData: PropTypes.func,
+  setRender: PropTypes.func
+}
+RecapItem.propTypes = {
+  showBackground: PropTypes.bool,
+  title: PropTypes.string,
+  count: PropTypes.number,
+  cards: PropTypes.array
 }
 
 export default SubEmploymentComponent
--- containers/Employment/SubEmployementContainer.jsx
+++ containers/Employment/SubEmployementContainer.jsx
@@ -6,36 +6,73 @@ import Layout from '@/components/core/Layout'
 import SubEmploymentComponent from '@/components/Employment/SubEmploymentComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps('recapComposition', 'recapASN', 'recapNonASN', 'recapOutsource'),
+  mapActions(
+    'getCompositionsCategories',
+    'getASNRecapByCategory',
+    'getNonASNRecapByCategory',
+    'getOutsourceRecapByCategory',
+  )
 )(
   class SubEmploymentContainer extends Component {
     static propTypes = {
       data: PropTypes.object,
-      banner: PropTypes.object,
-      getBanners: PropTypes.func,
-      deleteListBanner: PropTypes.func
+      recapComposition: PropTypes.object,
+      recapASN: PropTypes.object,
+      recapNonASN: PropTypes.object,
+      recapOutsource: PropTypes.object,
+      getCompositionsCategories: PropTypes.func,
+      getASNRecapByCategory: PropTypes.func,
+      getNonASNRecapByCategory: PropTypes.func,
+      getOutsourceRecapByCategory: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
-        willRender: false
+        willRender: true,
+        recapData: {}
       }
+      this.getRecapData = this.getRecapData.bind(this)
+      this.setRender = this.setRender.bind(this)
+      this.setRecapData = this.setRecapData.bind(this)
     }
 
-    componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+    componentDidMount() { }
+
+    setRecapData(data) {
+      this.setState({ recapData: data })
+    }
+
+    setRender(val) {
+      this.setState({ willRender: val })
+    }
+
+    getRecapData(router) {
+      const pagePath = router.asPath
+      const query = window.atob(router.query?.subEmployment)
+
+      if (pagePath?.includes('komposisi')) {
+        this.props.getCompositionsCategories(query)
+      } else if (pagePath?.includes('pegawai-asn')) {
+        this.props.getASNRecapByCategory(query)
+      } else if (pagePath?.includes('pegawai-non-asn')) {
+        this.props.getNonASNRecapByCategory(query)
+      } else if (pagePath?.includes('pegawai-outsourcing')) {
+        this.props.getOutsourceRecapByCategory(query)
+      }
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <SubEmploymentComponent {...this.state} {...this.props} />
+          <SubEmploymentComponent
+            {...this.state}
+            {...this.props}
+            getRecapData={this.getRecapData}
+            setRender={this.setRender}
+            setRecapData={this.setRecapData}
+          />
         </Layout>
       )
     }
