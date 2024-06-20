fc204a608339a86a9b727224ded0b691f57cf76d
azhar.rf <azhar@ekuator.id>
2024-06-20 16:15:37 +0700

    feat: options libs

---
+++ libs/types/options.js
@@ -0,0 +1,78 @@
+export const genderOptions = ['Laki-Laki', 'Perempuan']
+
+export const relationshipStatusOptions = ['Orang Tua', 'Anak']
+
+export const religionOptions = [
+  'Islam',
+  'Kristen',
+  'Katolik',
+  'Hindu',
+  'Buddha',
+  'Konghucu'
+]
+
+export const maritalStatusOptions = [
+  'Belum Menikah',
+  'Menikah',
+  'Cerai',
+  'Janda',
+  'Duda'
+]
+
+export const employeeStatusOptions = [
+  'Aktif',
+  'Pensiun',
+  'Berhenti',
+  'Meninggal',
+  'Alih Status',
+  'Aktif PS',
+  'CLTN',
+  'TBLN',
+  'Non Aktif',
+  'Hukdis'
+]
+
+export const employeeEducationLevelOptions = [
+  'SD/Sederajat',
+  'SLTP/Sederajat',
+  'SLTA/Sederajat',
+  'Akademik/D3/S.Muda',
+  'Diploma IV',
+  'Strata I',
+  'Strata II',
+  'Strata III'
+]
+
+export const educationStatusOptions = [
+  'Lulus',
+  'DO',
+  'Aktif',
+  'Non Aktif',
+  'Mengundurkan Diri'
+]
+
+export const periodOptions = ['Q1', 'Q2', 'Q3', 'Q4', 'Tahunan']
+
+export const organizationOptions = ['Kemensetneg']
+
+export const monthOptions = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember'
+]
+
+export const positionDescOptions = [
+  'Promosi',
+  'Mutasi',
+  'Inpassing',
+  'Konversi'
+]
