bfa513937e97cdd3c62dfe4c0153458c90a0db93
azhar.rf <azhar@ekuator.id>
2024-07-02 20:53:05 +0700

    feat: integration post employee Non ASN

--- containers/DataPegawaiContainer/Non-Asn/EmployeeAddNonASNContainer.jsx
+++ containers/DataPegawaiContainer/Non-Asn/EmployeeAddNonASNContainer.jsx
@@ -6,33 +6,94 @@ import Layout from '@/components/core/Layout'
 import EmployeeAddComponent from '@/components/DataPegawai/Non-Asn/EmployeeAddComponent'
 
 export default connect(
-  mapStateToProps(),
-  mapActions()
+  mapStateToProps(
+    'employee',
+    'position',
+    'echelon',
+    'grade',
+    'institution',
+    'residence',
+    'employmentType'
+  ),
+  mapActions(
+    'postEmployee',
+    'getPositions',
+    'getEchelonsOptions',
+    'getGradesOptions',
+    'getInstitutionsOptions',
+    'getResidences',
+    'getEmploymentTypes'
+  )
 )(
   class EmployeeAddNonASNContainer extends Component {
     static propTypes = {
-      data: PropTypes.object
+      employee: PropTypes.object,
+      position: PropTypes.object,
+      echelon: PropTypes.object,
+      grade: PropTypes.object,
+      institution: PropTypes.object,
+      residence: PropTypes.object,
+      employmentType: PropTypes.object,
+      postEmployee: PropTypes.func,
+      getPositions: PropTypes.func,
+      getEchelonsOptions: PropTypes.func,
+      getGradesOptions: PropTypes.func,
+      getInstitutionsOptions: PropTypes.func,
+      getResidences: PropTypes.func,
+      getEmploymentTypes: PropTypes.func
     }
 
     constructor(props) {
       super(props)
       this.state = {
+        queries: {
+          page: 1,
+          limit: 10000,
+          search: ''
+        },
         willRender: false
       }
+      this.fetch = this.fetch.bind(this)
+      this.fetchHierarchy = this.fetchHierarchy.bind(this)
+      this.setLoading = this.setLoading.bind(this)
+    }
+
+    fetch(queries) {
+      this.props.getPositions({ ...queries, filterParent: true, parentId: '' })
+      this.props.getEchelonsOptions(queries)
+      this.props.getGradesOptions(queries)
+      this.props.getInstitutionsOptions(queries)
+      this.props.getResidences(queries)
+      this.props.getEmploymentTypes({ ...queries, type: 2 })
+    }
+
+    fetchHierarchy(val) {
+      this.props.getPositions({
+        ...this.state.queries,
+        filterParent: true,
+        parentId: val
+      })
+    }
+
+    setLoading(val) {
+      this.setState({
+        willRender: val
+      })
     }
 
     componentDidMount() {
-      setTimeout(() => {
-        this.setState({
-          willRender: true
-        })
-      }, 2000)
+      this.fetch(this.state.queries)
     }
 
     render() {
       return (
         <Layout willRender={this.state.willRender}>
-          <EmployeeAddComponent {...this.state} {...this.props} />
+          <EmployeeAddComponent
+            onLoading={this.setLoading}
+            onFetchHierarchy={this.fetchHierarchy}
+            {...this.state}
+            {...this.props}
+          />
         </Layout>
       )
     }
