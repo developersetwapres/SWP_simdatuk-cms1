c6f4e13c007f816513de7d2c871040216c937e6c
Difa Fadhil J <difa.fadhil.juliansyah@gmail.com>
2024-09-02 11:29:12 +0700

    fix: handle null exception on dashboard

--- components/Dashboard/SectionDashboard/ContentCount.jsx
+++ components/Dashboard/SectionDashboard/ContentCount.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { Box, Card, Grid, Typography } from '@mui/material'
 import { v4 as uuidv4 } from 'uuid'
 
 const ContentCount = ({ data, datas }) => {
-  const counts = datas[data?.type]
-  const labels = data?.count?.map((e) => e?.title)
-  const transformed = !!counts && typeof counts === 'object' && Array.isArray(counts) ? counts : Object.entries(counts)
+  const counts = useMemo(() => {
+    return datas[data?.type]
+  }, [datas, data])
+
+  const labels = useMemo(() => {
+    return data?.count?.map((e) => e?.title)
+  }, [data])
+
+  const transformed = useMemo(() => {
+    if (data?.type === 'generations') {
+      return counts
+    }
+
+    return counts ? Object.entries(counts) : null
+  }, [counts, data?.type])
 
   return (
     <Box>
@@ -79,7 +91,6 @@ const CardCount = ({ title, count }) => {
         variant='p'
         component='p'
         sx={{
-          // width: '90%',
           marginBottom: '8px',
           fontSize: '16px',
           fontWeight: 440
@@ -104,6 +115,7 @@ CardCount.propTypes = {
   title: PropTypes.string,
   count: PropTypes.number
 }
+
 ContentCount.propTypes = {
   data: PropTypes.object,
   datas: PropTypes.object
